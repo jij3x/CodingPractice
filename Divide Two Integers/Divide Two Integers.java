cc65ce7b557a93f0651c9de30cfe2bba0a4f0c6e
jij3x <jijex@yahoo.com>
2013-11-29 08:40:37 -0800

    add comments for 'Divide Two Integers'

--- Divide Two Integers/Divide Two Integers.java
+++ Divide Two Integers/Divide Two Integers.java
@@ -4,12 +4,22 @@ public class Solution {
 			return 0;
 
 		int result = 0;
+
+		/*
+		 * Flip both dividend and divisor to negative to avoid overflow, as
+		 * 'int' range is [-2,147,483,648 to 2,147,483,647].
+		 */
 		boolean returnNeg = (dividend < 0 && divisor > 0)
 				|| (dividend > 0 && divisor < 0) ? true : false;
 		dividend = dividend < 0 ? dividend : 0 - dividend;
 		divisor = divisor < 0 ? divisor : 0 - divisor;
 
 		int factor = 1;
+		/*
+		 * 'divisor' will be increasing exponentially, we need a backup divisor
+		 * to determine when to stop. Also, since both dividend and div are
+		 * negative here, stop when dividend 'greater than' div.
+		 */
 		long div = divisor;
 		while (dividend <= div) {
 			if (divisor < dividend && factor > 1) {
@@ -18,8 +28,13 @@ public class Solution {
 				continue;
 			}
 
+			/*
+			 * Increase divisor by factor 2. Stop increasing when it reaches to
+			 * half of MIN_VALUE, otherwise it will overflow later when it
+			 * doubles.
+			 */
 			while ((dividend <= (divisor << 1))
-					&& (Integer.MIN_VALUE >> 2 < divisor)) {
+					&& (Integer.MIN_VALUE >> 1 < divisor)) {
 				divisor <<= 1;
 				factor <<= 1;
 			}
