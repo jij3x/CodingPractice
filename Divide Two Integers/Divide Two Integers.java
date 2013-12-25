e9c7ee8665ca5b3791754be2f5d0fe5487952fbe
jij3x <jijex@yahoo.com>
2013-12-25 10:26:35 -0800

    Update Divide Two Integers.java

--- Divide Two Integers/Divide Two Integers.java
+++ Divide Two Integers/Divide Two Integers.java
@@ -9,8 +9,7 @@ public class Solution {
 		 * Flip both dividend and divisor to negative to avoid overflow, as
 		 * 'int' range is [-2,147,483,648 to 2,147,483,647].
 		 */
-		boolean returnNeg = (dividend < 0 && divisor > 0)
-				|| (dividend > 0 && divisor < 0) ? true : false;
+		boolean returnNeg = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0) ? true : false;
 		dividend = dividend < 0 ? dividend : 0 - dividend;
 		divisor = divisor < 0 ? divisor : 0 - divisor;
 
@@ -33,8 +32,7 @@ public class Solution {
 			 * half of MIN_VALUE, otherwise it will overflow later when it
 			 * doubles.
 			 */
-			while ((dividend <= (divisor << 1))
-					&& (Integer.MIN_VALUE >> 1 < divisor)) {
+			while ((dividend <= (divisor << 1)) && (Integer.MIN_VALUE >> 1 < divisor)) {
 				divisor <<= 1;
 				factor <<= 1;
 			}
