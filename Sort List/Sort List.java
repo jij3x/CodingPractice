163e66741482d414bee1a57f1a25072f878d1db2
jij3x <jijex@yahoo.com>
2013-12-24 16:16:34 -0800

    back to readable code

--- Sort List/Sort List.java
+++ Sort List/Sort List.java
@@ -14,11 +14,15 @@ public class Solution {
 		if (head == null || head.next == null)
 			return head;
 
-		ListNode rightHead = head, leftTail = head;
-		for (ListNode p = head; p != null; p = p == null ? null : p.next) {
+		ListNode rightHead = head;
+		ListNode leftTail = head;
+		ListNode pioneer = head;
+		while (pioneer != null) {
 			leftTail = rightHead;
 			rightHead = rightHead.next;
-			p = p.next;
+			pioneer = pioneer.next;
+			if (pioneer != null)
+				pioneer = pioneer.next;
 		}
 		leftTail.next = null;
 
@@ -27,12 +31,22 @@ public class Solution {
 
 		ListNode start = new ListNode(0);
 		start.next = head;
-		for (ListNode prev = start; head != null || rightHead != null; prev = prev.next) {
-			boolean leftIn = (rightHead == null || (head != null && rightHead != null && head.val < rightHead.val));
-			prev.next = leftIn ? head : rightHead;
-			head = leftIn ? head.next : head;
-			rightHead = leftIn ? rightHead : rightHead.next;
+		ListNode prev = start;
+		while (head != null && rightHead != null) {
+			if (head.val > rightHead.val) {
+				prev.next = head;
+				head = head.next;
+			} else {
+				prev.next = rightHead;
+				rightHead = rightHead.next;
+			}
+			prev = prev.next;
 		}
+		if (head == null)
+			prev.next = rightHead;
+		else
+			prev.next = head;
+
 		return start.next;
 	}
 }
