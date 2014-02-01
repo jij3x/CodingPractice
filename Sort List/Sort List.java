95e5a9647e2aa9d4990f35a346dedb1be25d4e3f
jij3x <jijex@yahoo.com>
2014-02-01 12:35:25 -0800

    Update Sort List.java

--- Sort List/Sort List.java
+++ Sort List/Sort List.java
@@ -49,4 +49,30 @@ public class Solution {
 
 		return start.next;
 	}
+
+	public ListNode sortList2(ListNode head) {
+		if (head == null || head.next == null)
+			return head;
+
+		ListNode rightHead = head, leftTail = head;
+		for (ListNode p = head; p != null; p = p == null ? null : p.next) {
+			leftTail = rightHead;
+			rightHead = rightHead.next;
+			p = p.next;
+		}
+		leftTail.next = null;
+
+		head = sortList(head);
+		rightHead = sortList(rightHead);
+
+		ListNode start = new ListNode(0);
+		start.next = head;
+		for (ListNode prev = start; head != null || rightHead != null; prev = prev.next) {
+			boolean leftIn = (rightHead == null || (head != null && rightHead != null && head.val < rightHead.val));
+			prev.next = leftIn ? head : rightHead;
+			head = leftIn ? head.next : head;
+			rightHead = leftIn ? rightHead : rightHead.next;
+		}
+		return start.next;
+	}
 }
