9a59aa6da3753d03bf990012f988518be017a845
jij3x <jijex@yahoo.com>
2014-02-01 11:19:50 -0800

    Update Gray Code.java

--- Gray Code/Gray Code.java
+++ Gray Code/Gray Code.java
@@ -2,7 +2,7 @@ public class Solution {
 	public ArrayList<Integer> grayCode(int n) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
 
-		for (int i = 0; i < (1<<n); i++)
+		for (int i = 0; i < (1 << n); i++)
 			result.add((i >> 1) ^ i);
 
 		return result;
