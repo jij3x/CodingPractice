62f4e2b04ef7153afd9320594a03ea41f65e91da
jij3x <jijex@yahoo.com>
2014-02-01 14:17:38 -0800

    upload

--- Sort List/Sort List.java
+++ Sort List/Sort List.java
@@ -66,7 +66,6 @@ public class Solution {
 		rightHead = sortList(rightHead);
 
 		ListNode start = new ListNode(0);
-		start.next = head;
 		for (ListNode prev = start; head != null || rightHead != null; prev = prev.next) {
 			boolean leftIn = (rightHead == null || (head != null && rightHead != null && head.val < rightHead.val));
 			prev.next = leftIn ? head : rightHead;
@@ -74,5 +73,59 @@ public class Solution {
 			rightHead = leftIn ? rightHead : rightHead.next;
 		}
 		return start.next;
+	}
+}
+
+class Solution2 {
+	public ListNode sortList(ListNode head) {
+		ListNode start = new ListNode(0);
+		start.next = head;
+
+		int length = 0;
+		for (; head != null; length++)
+			head = head.next;
+
+		if (length > 1) {
+			int segment = 1;
+			while (length > segment) {
+				ListNode prev = start;
+				while (prev.next != null) {
+					ListNode list1 = prev.next;
+					ListNode list2 = cutList(list1, segment);
+					ListNode rest = cutList(list2, segment);
+
+					ListNode[] headAndTail = new ListNode[2];
+					mergeList(list1, list2, headAndTail);
+					prev.next = headAndTail[0];
+					prev = headAndTail[1];
+					prev.next = rest;
+				}
+				segment *= 2;
+			}
+		}
+		return start.next;
+	}
+
+	private ListNode cutList(ListNode head, int n) {
+		if (head == null)
+			return null;
+
+		for (int i = 1; i < n && head.next != null; i++)
+			head = head.next;
+		ListNode tail = head.next;
+		head.next = null;
+		return tail;
+	}
+
+	private void mergeList(ListNode list1, ListNode list2, ListNode[] headAndTail) {
+		ListNode start = new ListNode(0);
+		headAndTail[1] = start;
+		for (; list1 != null || list2 != null; headAndTail[1] = headAndTail[1].next) {
+			boolean list1In = (list2 == null || (list1 != null && list2 != null && list1.val < list2.val));
+			headAndTail[1].next = list1In ? list1 : list2;
+			list1 = list1In ? list1.next : list1;
+			list2 = list1In ? list2 : list2.next;
+		}
+		headAndTail[0] = start.next;
 	}
 }
