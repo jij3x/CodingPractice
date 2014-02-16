ca1ba1634f08d6a49da3ac3c71568b89e4fe168f
jij3x <jijex@yahoo.com>
2014-02-16 11:58:10 -0800

    update

--- Valid Number/Valid Number.java
+++ Valid Number/Valid Number.java
@@ -1,6 +1,76 @@
 public class Solution {
 	public boolean isNumber(String s) {
 		s = s.trim();
+		if (isInteger(s))
+			return true;
+		else if (isFloat(s))
+			return true;
+		else if (isENotion(s))
+			return true;
+
+		return false;
+	}
+
+	private boolean isInteger(String s) {
+		if (!s.isEmpty() && (s.charAt(0) == '+' || s.charAt(0) == '-'))
+			s = s.substring(1);
+
+		if (s.isEmpty() || !allDigits(s))
+			return false;
+
+		return true;
+	}
+
+	private boolean isFloat(String s) {
+		if (!s.isEmpty() && (s.charAt(0) == '+' || s.charAt(0) == '-'))
+			s = s.substring(1);
+
+		int dotPos = findFirstDot(s);
+		if (dotPos == -1)
+			return false;
+
+		return s.length() > 1 && allDigits(s.substring(0, dotPos)) && allDigits(s.substring(dotPos + 1));
+	}
+
+	private boolean isENotion(String s) {
+		int ePos = findFirstE(s);
+		if (ePos == -1)
+			return false;
+
+		return (isInteger(s.substring(0, ePos)) || isFloat(s.substring(0, ePos))) && isInteger(s.substring(ePos + 1));
+	}
+
+	private boolean allDigits(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) < '0' || s.charAt(i) > '9')
+				return false;
+		}
+		return true;
+	}
+
+	private int findFirstDot(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '.')
+				return i;
+		}
+		return -1;
+	}
+
+	private int findFirstE(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'e' || s.charAt(i) == 'E')
+				return i;
+		}
+		return -1;
+	}
+}
+
+/*
+ * DFA
+ */
+class Solution2 {
+	public boolean isNumber(String s) {
+		s = s.trim();
 
 		/*
 		 *     | +/- | 0-9 | e/E | .
