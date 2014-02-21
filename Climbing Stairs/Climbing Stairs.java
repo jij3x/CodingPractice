a759dbf90d285b3bdd737a7af78d8cb5363f5f8e
jij3x <jijex@yahoo.com>
2014-02-20 19:56:56 -0800

    upload

---
+++ Climbing Stairs/Climbing Stairs.java
@@ -0,0 +1,32 @@
+public class Solution {
+	public int climbStairs(int n) {
+		int[] memo = new int[n + 1];
+		Arrays.fill(memo, -1);
+		return dpClimbStairs(n, memo);
+	}
+
+	private int dpClimbStairs(int n, int[] memo) {
+		if (n == 0 || n == 1)
+			return 1;
+
+		if (memo[n] == -1)
+			memo[n] = dpClimbStairs(n - 1, memo) + dpClimbStairs(n - 2, memo);
+
+		return memo[n];
+	}
+}
+
+class Solution2 {
+	public int climbStairs(int n) {
+		if (n == 0 || n == 1)
+			return 1;
+
+		int prev = 1, prevPrev = 1, result = 0;
+		for (int i = 2; i <= n; i++) {
+			result = prev + prevPrev;
+			prevPrev = prev;
+			prev = result;
+		}
+		return result;
+	}
+}
