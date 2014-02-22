c02e946aad0f0b114ec1eb0e0ef7d7fc0da00b1d
jij3x <jijex@yahoo.com>
2014-02-22 15:30:26 -0800

    update

--- Reverse Linked List II/Reverse Linked List II.java
+++ Reverse Linked List II/Reverse Linked List II.java
@@ -14,18 +14,39 @@ public class Solution {
 		ListNode start = new ListNode(0);
 		start.next = head;
 		ListNode prev = start;
-
-		for (int i = 1; i < m && prev.next != null; i++)
+		for (int i = 1; i < m; i++)
 			prev = prev.next;
 
-		ListNode pivot = prev.next;
-		for (int i = m; i < n && pivot.next != null; i++) {
-			ListNode second = pivot.next;
-			pivot.next = second.next;
-			second.next = prev.next;
-			prev.next = second;
+		ListNode curr = prev.next;
+		for (int i = m; i < n; i++) {
+			ListNode next = curr.next;
+			curr.next = next.next;
+			next.next = prev.next;
+			prev.next = next;
 		}
-		
+
+		return start.next;
+	}
+}
+
+class Solution2 {
+	public ListNode reverseBetween(ListNode head, int m, int n) {
+		ListNode start = new ListNode(0);
+		start.next = head;
+		ListNode prev = start;
+		for (int i = 1; i < m; i++)
+			prev = prev.next;
+
+		ListNode subTail = prev.next, subHead = prev, next = prev.next;
+		for (int i = m; i <= n; i++) {
+			ListNode temp = next.next;
+			next.next = subHead;
+			subHead = next;
+			next = temp;
+		}
+
+		prev.next = subHead;
+		subTail.next = next;
 		return start.next;
 	}
 }
