b136e5c7f0bc403e7bba1e2d6c81b1c9ca27612a
jij3x <jijex@yahoo.com>
2014-02-23 07:30:04 -0800

    Update Search for a Range.java

--- Search for a Range/Search for a Range.java
+++ Search for a Range/Search for a Range.java
@@ -1,41 +1,38 @@
 public class Solution {
 	public int[] searchRange(int[] A, int target) {
-		int[] result = { -1, -1 };
+		int[] range = { -1, -1 };
 
 		int start = 0, end = A.length - 1;
 		while (start <= end) {
 			int mid = (start + end) / 2;
-			if (A[mid] == target) {
+			if (A[mid] == target)
 				if (mid == 0 || A[mid - 1] < target) {
-					result[0] = mid;
+					range[0] = mid;
 					break;
 				} else {
 					end = mid - 1;
 				}
-			} else if (A[mid] < target) {
+			else if (A[mid] < target)
 				start = mid + 1;
-			} else {
+			else
 				end = mid - 1;
-			}
 		}
 
 		end = A.length - 1;
 		while (start <= end) {
 			int mid = (start + end) / 2;
-			if (A[mid] == target) {
+			if (A[mid] == target)
 				if (mid + 1 == A.length || A[mid + 1] > target) {
-					result[1] = mid;
+					range[1] = mid;
 					break;
 				} else {
 					start = mid + 1;
 				}
-			} else if (A[mid] < target) {
+			else if (A[mid] < target)
 				start = mid + 1;
-			} else {
+			else
 				end = mid - 1;
-			}
 		}
-
-		return result;
+		return range;
 	}
 }
