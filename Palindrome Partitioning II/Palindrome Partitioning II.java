39aaaced6dffb005bf72ca61bcf6902c6ac7ea10
jij3x <jijex@yahoo.com>
2014-03-06 00:07:56 -0800

    update

--- Palindrome Partitioning II/Palindrome Partitioning II.java
+++ Palindrome Partitioning II/Palindrome Partitioning II.java
@@ -2,11 +2,9 @@ public class Solution {
 	public int minCut(String s) {
 		boolean[][] palindromeMemo = new boolean[s.length()][s.length()];
 		int[] minCutMemo = new int[s.length()];
-		for (int i = 0; i < s.length(); i++) {
-			minCutMemo[i] = i + 1;
-		}
 
 		for (int i = 0; i < s.length(); i++) {
+			minCutMemo[i] = i + 1;
 			for (int j = i; j >= 0; j--) {
 				if (j == i || (s.charAt(j) == s.charAt(i) && (j == i - 1 || palindromeMemo[j + 1][i - 1]))) {
 					palindromeMemo[j][i] = true;
@@ -18,42 +16,42 @@ public class Solution {
 	}
 }
 
-/*
- * Very slow.
- */
 class Solution2 {
 	public int minCut(String s) {
 		boolean[][] palindromeMemo = new boolean[s.length()][s.length()];
-		palindromeMemo[s.length() - 1][s.length() - 1] = true;
-		for (int i = 0; i < s.length() - 1; i++) {
-			palindromeMemo[i][i] = true;
-			palindromeMemo[i][i + 1] = s.charAt(i) == s.charAt(i + 1);
-		}
-		for (int i = 2; i < s.length(); i++) {
-			for (int j = i - 2; j >= 0; j--) {
-				palindromeMemo[j][i] = (s.charAt(j) == s.charAt(i)) && palindromeMemo[j + 1][i - 1];
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i; j >= 0; j--) {
+				if (s.charAt(j) == s.charAt(i) && (i - j <= 1 || palindromeMemo[j + 1][i - 1]))
+					palindromeMemo[j][i] = true;
 			}
 		}
 
-		int[][] minCutMemo = new int[s.length()][s.length()];
-		findMinCut(s, 0, s.length() - 1, minCutMemo, palindromeMemo);
-		return minCutMemo[0][s.length() - 1];
-	}
+		Queue<Integer> workingQue = new LinkedList<Integer>();
+		boolean[] visited = new boolean[s.length() + 1];
+		workingQue.add(0);
+		visited[0] = true;
+		int currLvlCnt = 1, nextLvlCnt = 0, level = 0;
+		while (currLvlCnt > 0) {
+			int ps = workingQue.poll();
+			currLvlCnt--;
+			if (ps == s.length())
+				return level - 1;
 
-	private void findMinCut(String s, int start, int end, int[][] minCutMemo, boolean[][] palindromeMemo) {
-		if (minCutMemo[start][end] != 0)
-			return;
-		if (palindromeMemo[start][end]) {
-			minCutMemo[start][end] = 1;
-			return;
+			for (int i = ps; i < s.length(); i++) {
+				if (palindromeMemo[ps][i] && !visited[i + 1]) {
+					workingQue.add(i + 1);
+					visited[i + 1] = true;
+					nextLvlCnt++;
+				}
+			}
+
+			if (currLvlCnt == 0) {
+				currLvlCnt = nextLvlCnt;
+				nextLvlCnt = 0;
+				level++;
+			}
 		}
 
-		int minCut = end - start + 1;
-		for (int i = start; i < end; i++) {
-			findMinCut(s, start, i, minCutMemo, palindromeMemo);
-			findMinCut(s, i + 1, end, minCutMemo, palindromeMemo);
-			minCut = Math.min(minCut, minCutMemo[start][i] + minCutMemo[i + 1][end]);
-		}
-		minCutMemo[start][end] = minCut;
+		return 0;
 	}
 }
