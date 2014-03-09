8a70eb8f0a9d098e75f53ad3b0246ae26a5fd269
jij3x <jijex@yahoo.com>
2014-03-08 20:45:19 -0800

    upload

---
+++ Longest Palindromic Substring/Longest Palindromic Substring.java
@@ -0,0 +1,45 @@
+public class Solution {
+	public String longestPalindrome(String s) {
+		int maxLen = 0, maxPos = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int len = palindromeLengthAround(s, i, i);
+			if (len > maxLen) {
+				maxLen = len;
+				maxPos = i - (len - 1) / 2;
+			}
+			len = palindromeLengthAround(s, i, i + 1);
+			if (len > maxLen) {
+				maxLen = len;
+				maxPos = i - (len - 2) / 2;
+			}
+		}
+		return s.substring(maxPos, maxPos + maxLen);
+	}
+
+	private int palindromeLengthAround(String s, int left, int right) {
+		int len = 0;
+		while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+			len = (right++ - left--) + 1;
+		}
+		return len;
+	}
+}
+
+class Solution2 {
+	public String longestPalindrome(String s) {
+		boolean[][] memo = new boolean[s.length()][s.length()];
+		int maxLen = 0, maxPos = 0;
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i; j >= 0; j--) {
+				if (s.charAt(i) == s.charAt(j) && (i - j <= 1 || memo[j + 1][i - 1])) {
+					memo[j][i] = true;
+					if (i - j + 1 > maxLen) {
+						maxLen = i - j + 1;
+						maxPos = j;
+					}
+				}
+			}
+		}
+		return s.substring(maxPos, maxPos + maxLen);
+	}
+}
