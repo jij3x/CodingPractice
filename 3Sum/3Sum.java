41adcb9bc401674247795f37c0126092ef0a9033
jij3x <jijex@yahoo.com>
2014-04-08 00:27:17 -0700

    Update 3Sum.java

--- 3Sum/3Sum.java
+++ 3Sum/3Sum.java
@@ -1,32 +1,31 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
-		Arrays.sort(num);
-		
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		for (int i = 0; i <= num.length - 3; i++) {
-			int left = i + 1, right = num.length - 1;
-			while (left < right) {
-				if (num[left] + num[right] == -num[i]) {
-					ArrayList<Integer> subResult = new ArrayList<Integer>();
-					subResult.add(num[i]);
-					subResult.add(num[left++]);
-					subResult.add(num[right--]);
-					result.add(subResult);
+    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        Arrays.sort(num);
 
-					while (left < right && num[left] == num[left - 1])
-						left++;
-					while (left < right && num[right] == num[right + 1])
-						right--;
-				} else if (num[left] + num[right] > -num[i]) {
-					right--;
-				} else {
-					left++;
-				}
-			}
+        for (int i = 0; i < num.length - 2; i++) {
+            if (i > 0 && num[i] == num[i - 1])
+                continue;
 
-			while (i < num.length - 1 && num[i] == num[i + 1])
-				i++;
-		}
-		return result;
-	}
+            int m = i + 1, n = num.length - 1;
+            while (m < n) {
+                if (num[m] + num[n] < -num[i]) {
+                    m++;
+                } else if (num[m] + num[n] > -num[i]) {
+                    n--;
+                } else {
+                    ArrayList<Integer> r = new ArrayList<Integer>(Arrays.asList(num[i], num[m++], num[n--]));
+                    result.add(r);
+
+                    while (m < n && num[m] == num[m - 1])
+                        m++;
+
+                    while (m < n && num[n] == num[n + 1])
+                        n--;
+                }
+            }
+        }
+
+        return result;
+    }
 }
