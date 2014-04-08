dd35725b7b613e82913dd064a5ff8d636ff00e1d
jij3x <jijex@yahoo.com>
2014-04-08 15:56:59 -0700

    Update Clone Graph.java

--- Clone Graph/Clone Graph.java
+++ Clone Graph/Clone Graph.java
@@ -7,31 +7,27 @@
  * };
  */
 public class Solution {
-	public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-		if (node == null)
-			return null;
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null)
+            return null;
 
-		HashMap<Integer, UndirectedGraphNode> targets = new HashMap<Integer, UndirectedGraphNode>();
-		Queue<UndirectedGraphNode> workingQue = new LinkedList<UndirectedGraphNode>();
+        Map<UndirectedGraphNode, UndirectedGraphNode> visited = new HashMap<UndirectedGraphNode, UndirectedGraphNode>();
+        Queue<UndirectedGraphNode> que = new LinkedList<UndirectedGraphNode>();
+        que.add(node);
+        visited.put(node, new UndirectedGraphNode(node.label));
+        while (!que.isEmpty()) {
+            UndirectedGraphNode curr = que.poll();
+            for (UndirectedGraphNode n : curr.neighbors) {
+                if (!visited.containsKey(n)) {
+                    que.add(n);
+                    visited.put(n, new UndirectedGraphNode(n.label));
+                }
+                visited.get(curr).neighbors.add(visited.get(n));
+            }
+        }
 
-		workingQue.add(node);
-		UndirectedGraphNode returnNode = new UndirectedGraphNode(node.label);
-		targets.put(node.label, returnNode);
-		while (!workingQue.isEmpty()) {
-			UndirectedGraphNode s = workingQue.poll();
-			UndirectedGraphNode t = targets.get(s.label);
-			for (int i = 0; i < s.neighbors.size(); i++) {
-				UndirectedGraphNode sn = s.neighbors.get(i);
-				if (!targets.containsKey(sn.label)) {
-					workingQue.add(sn);
-					targets.put(sn.label, new UndirectedGraphNode(sn.label));
-				}
-				t.neighbors.add(targets.get(sn.label));
-			}
-		}
-
-		return returnNode;
-	}
+        return visited.get(node);
+    }
 }
 
 class Solution2 {
