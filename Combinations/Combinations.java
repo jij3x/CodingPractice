2b94463f154011503bc0733e631a6153baffad05
jij3x <jijex@yahoo.com>
2014-04-09 00:03:07 -0700

    Update Combinations.java

--- Combinations/Combinations.java
+++ Combinations/Combinations.java
@@ -1,27 +1,52 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> combine(int n, int k) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		if (k > n) {
-			return result;
-		} else if (k == 1) { // base case needs to be on (k=1), rather than (k=n), to prevent k go below 0
-			for (int i = 1; i <= n; i++) {
-				ArrayList<Integer> row = new ArrayList<Integer>();
-				row.add(i);
-				result.add(row);
-			}
-			return result;
-		}
+    public ArrayList<ArrayList<Integer>> combine(int n, int k) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> path = new ArrayList<Integer>();
 
-		ArrayList<ArrayList<Integer>> subResult = combine(n - 1, k - 1);
-		for (ArrayList<Integer> row : subResult) {
-			row.add(n);
-			Collections.sort(row);
-			result.add(row);
-		}
+        doCombine(n, k, path, result);
+        return result;
+    }
 
-		subResult = combine(n - 1, k);
-		result.addAll(subResult);
+    private void doCombine(int n, int k, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> result) {
+        if (k == 0) {
+            ArrayList<Integer> r = new ArrayList<Integer>(path);
+            Collections.sort(r);
+            result.add(r);
+            return;
+        }
 
-		return result;
-	}
+        for (int i = n; i >= k; i--) {
+            path.add(i);
+            doCombine(i - 1, k - 1, path, result);
+            path.remove(path.size() - 1);
+        }
+    }
+}
+
+class Solution2 {
+    public ArrayList<ArrayList<Integer>> combine(int n, int k) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (k > n) {
+            return result;
+        } else if (k == 1) {
+            for (int i = 1; i <= n; i++) {
+                ArrayList<Integer> row = new ArrayList<Integer>();
+                row.add(i);
+                result.add(row);
+            }
+            return result;
+        }
+
+        ArrayList<ArrayList<Integer>> subResult = combine(n - 1, k - 1);
+        for (ArrayList<Integer> row : subResult) {
+            row.add(n);
+            Collections.sort(row);
+            result.add(row);
+        }
+
+        subResult = combine(n - 1, k);
+        result.addAll(subResult);
+
+        return result;
+    }
 }
