34a8e6c245931e5016ad8817f0ad542bfe3ef949
jij3x <jijex@yahoo.com>
2014-04-09 22:42:12 -0700

    Update Combination Sum.java

--- Combination Sum/Combination Sum.java
+++ Combination Sum/Combination Sum.java
@@ -1,26 +1,23 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
-		return subCombinationSum(candidates, 0, target);
-	}
+    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+        return subCombinationSum(candidates, target, 0);
+    }
 
-	public ArrayList<ArrayList<Integer>> subCombinationSum(int[] candidates, int start, int target) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    private ArrayList<ArrayList<Integer>> subCombinationSum(int[] candidates, int target, int idx) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 
-		for (int i = start; i < candidates.length; i++) {
-			if (candidates[i] == target) {
-				ArrayList<Integer> row = new ArrayList<Integer>();
-				row.add(candidates[i]);
-				result.add(row);
-			} else if (candidates[i] < target) {
-				ArrayList<ArrayList<Integer>> subResult = subCombinationSum(candidates, i, target - candidates[i]);
-				for (ArrayList<Integer> row : subResult) {
-					row.add(candidates[i]);
-					Collections.sort(row);
-					result.add(row);
-				}
-			}
-		}
+        for (int i = idx; i < candidates.length; i++) {
+            if (candidates[i] == target) {
+                result.add(new ArrayList<Integer>(Arrays.asList(candidates[i])));
+            } else if (candidates[i] < target) {
+                for (ArrayList<Integer> r : subCombinationSum(candidates, target - candidates[i], i)) {
+                    r.add(candidates[i]);
+                    Collections.sort(r);
+                    result.add(r);
+                }
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 }
