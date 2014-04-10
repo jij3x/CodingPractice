b63e146651be28872725c3873bccd1d8a8e754e5
jij3x <jijex@yahoo.com>
2014-04-09 22:52:07 -0700

    Update Combination Sum.java

--- Combination Sum/Combination Sum.java
+++ Combination Sum/Combination Sum.java
@@ -21,3 +21,28 @@ public class Solution {
         return result;
     }
 }
+
+class Solution2 {
+    public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        subCombinationSum(candidates, target, 0, path, result);
+        return result;
+    }
+
+    private void subCombinationSum(int[] candidates, int target, int idx, ArrayList<Integer> path,
+            ArrayList<ArrayList<Integer>> result) {
+        for (int i = idx; i < candidates.length; i++) {
+            if (candidates[i] == target) {
+                ArrayList<Integer> r = new ArrayList<Integer>(path);
+                r.add(candidates[i]);
+                Collections.sort(r);
+                result.add(r);
+            } else if (candidates[i] < target) {
+                path.add(candidates[i]);
+                subCombinationSum(candidates, target - candidates[i], i, path, result);
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+}
