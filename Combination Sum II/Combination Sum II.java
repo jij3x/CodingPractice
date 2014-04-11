e273514d4e724fe03f5db19a6329cd5397a9004f
jij3x <jijex@yahoo.com>
2014-04-10 21:44:32 -0700

    Update Combination Sum II.java

--- Combination Sum II/Combination Sum II.java
+++ Combination Sum II/Combination Sum II.java
@@ -1,31 +1,27 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> combinationSum2(int[] num, int target) {
-		Arrays.sort(num);
-		return subCombinationSum(num, 0, target);
-	}
+    public ArrayList<ArrayList<Integer>> combinationSum2(int[] num, int target) {
+        Arrays.sort(num);
+        return subCombinationSum2(num, target, 0);
+    }
 
-	private ArrayList<ArrayList<Integer>> subCombinationSum(int[] num, int start, int target) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    private ArrayList<ArrayList<Integer>> subCombinationSum2(int[] num, int target, int idx) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
 
-		for (int i = start; i < num.length; i++) {
-			if (num[i] == target) {
-				ArrayList<Integer> row = new ArrayList<Integer>();
-				row.add(num[i]);
-				result.add(row);
-			} else if (num[i] < target) {
-				ArrayList<ArrayList<Integer>> subResult = new ArrayList<ArrayList<Integer>>();
-				subResult = subCombinationSum(num, i + 1, target - num[i]);
-				for (ArrayList<Integer> row : subResult) {
-					row.add(num[i]);
-					Collections.sort(row);
-					result.add(row);
-				}
-			}
+        for (int i = idx; i < num.length; i++) {
+            if (num[i] == target) {
+                result.add(new ArrayList<Integer>(Arrays.asList(num[i])));
+            } else if (num[i] < target) {
+                for (ArrayList<Integer> r : subCombinationSum2(num, target - num[i], i + 1)) {
+                    r.add(num[i]);
+                    Collections.sort(r);
+                    result.add(r);
+                }
+            }
 
-			while (i < num.length - 1 && num[i] == num[i + 1])
-				i++;
-		}
+            while (i + 1 < num.length && num[i] == num[i + 1])
+                i++;
+        }
 
-		return result;
-	}
+        return result;
+    }
 }
