89ce338a5bf6755b926ce1810837f7d29be6f511
jij3x <jijex@yahoo.com>
2014-04-12 21:32:57 -0700

    Update Pow(x, n).java

--- Pow(x, n)/Pow(x, n).java
+++ Pow(x, n)/Pow(x, n).java
@@ -1,29 +1,29 @@
 public class Solution {
-	public double pow(double x, int n) {
-		if (n == 0)
-			return 1;
+    public double pow(double x, int n) {
+        if (n == 0)
+            return 1;
 
-		double half = pow(x, n / 2);
-		if ((n & 1) == 0)
-			return half * half;
-		else if (n > 0)
-			return half * half * x;
-		else
-			return half * half / x;
-	}
+        double half = pow(x, n / 2);
+        if (n % 2 == 0)
+            return half * half;
+        else if (n > 0)
+            return half * half * x;
+        else
+            return half * half / x;
+    }
 }
 
 class Solution2 {
-	public double pow(double x, int n) {
-		int nn = n;
-		nn = Math.abs(nn);
-		double result = 1.0;
-		while (nn > 0) {
-			if ((nn & 1) == 1)
-				result *= x;
-			x *= x;
-			nn >>= 1;
-		}
-		return n < 0 ? 1.0 / result : result;
-	}
+    public double pow(double x, int n) {
+        int nn = Math.abs(n);
+        double result = 1.0;
+        while (nn > 0) {
+            if ((nn & 1) == 1)
+                result *= x;
+            x *= x;
+            nn >>= 1;
+        }
+
+        return n > 0 ? result : 1.0 / result;
+    }
 }
