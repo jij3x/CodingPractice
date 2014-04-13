5e4bb6e3cba147c77fdc0767f88b827001a32d75
jij3x <jijex@yahoo.com>
2014-04-12 20:50:35 -0700

    Update Subsets.java

--- Subsets/Subsets.java
+++ Subsets/Subsets.java
@@ -1,47 +1,39 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> subsets(int[] S) {
-		return subSubsets(S, 0);
-	}
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        result.add(new ArrayList<Integer>());
 
-	private ArrayList<ArrayList<Integer>> subSubsets(int[] S, int start) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		ArrayList<Integer> emptyRow = new ArrayList<Integer>();
-		result.add(emptyRow);
-		
-		if (start >= S.length)
-			return result;
-
-		for (int i = start; i < S.length; i++) {
-			ArrayList<ArrayList<Integer>> subResult = subSubsets(S, i + 1);
-			for (ArrayList<Integer> row : subResult) {
-				row.add(S[i]);
-				Collections.sort(row);
-				result.add(row);
-			}
-		}
-		return result;
-	}
+        Arrays.sort(S);
+        for (int i = 0; i < S.length; i++) {
+            int len = result.size();
+            for (int j = 0; j < len; j++) {
+                ArrayList<Integer> r = new ArrayList<Integer>(result.get(j));
+                r.add(S[i]);
+                result.add(r);
+            }
+        }
+        return result;
+    }
 }
 
 class Solution2 {
-	public ArrayList<ArrayList<Integer>> subsets(int[] S) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+        return doSubset(S, 0);
+    }
 
-		for (int i = 0; i < S.length; i++) {
-			int j = result.size() - 1;
-			while (j >= 0) {
-				ArrayList<Integer> row = new ArrayList<Integer>(result.get(j));
-				row.add(S[i]);
-				result.add(row);
-				j--;
-			}
+    private ArrayList<ArrayList<Integer>> doSubset(int[] S, int idx) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        result.add(new ArrayList<Integer>());
+        if (idx == S.length)
+            return result;
 
-			ArrayList<Integer> row = new ArrayList<Integer>();
-			row.add(S[i]);
-			result.add(row);
-		}
-
-		result.add(new ArrayList<Integer>());
-		return result;
-	}
+        for (int i = idx; i < S.length; i++) {
+            for (ArrayList<Integer> r : doSubset(S, i + 1)) {
+                r.add(S[i]);
+                Collections.sort(r);
+                result.add(r);
+            }
+        }
+        return result;
+    }
 }
