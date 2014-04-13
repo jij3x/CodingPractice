bfeff8502a8073c1cd581e57b91c4c461a34d439
jij3x <jijex@yahoo.com>
2014-04-12 22:20:57 -0700

    Update Sqrt(x).java

--- Sqrt(x)/Sqrt(x).java
+++ Sqrt(x)/Sqrt(x).java
@@ -1,15 +1,26 @@
 public class Solution {
-	public int sqrt(int x) {
-		int start = 0, end = x;
-		while (start <= end) {
-			long middle = ((long) start + (long) end) / 2;
-			if (middle * middle == x)
-				return (int) middle;
-			else if (middle * middle > x)
-				end = (int) middle - 1;
-			else
-				start = (int) middle + 1;
-		}
-		return end;
-	}
+    public int sqrt(int x) {
+        long start = 1, end = x;
+        while (start <= end) {
+            long mid = (start + end) / 2;
+            if (mid * mid == x)
+                return (int) mid;
+            else if (mid * mid > x)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+
+        return (int) end;
+    }
+}
+
+class Solution2 {
+    public int sqrt(int x) {
+        double epsilon = 1e-10, r = x;
+        while (Math.abs(r - x / r) > epsilon * r)
+            r = (x / r + r) / 2.0;
+
+        return (int) r;
+    }
 }
