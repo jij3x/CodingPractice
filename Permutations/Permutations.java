e6aed31c06b5b5763188eb4d050d6c839cc6ffc9
jij3x <jijex@yahoo.com>
2014-04-12 19:15:55 -0700

    Update Permutations.java

--- Permutations/Permutations.java
+++ Permutations/Permutations.java
@@ -1,25 +1,25 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> permute(int[] num) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		boolean[] visited = new boolean[num.length];
-		ArrayList<Integer> path = new ArrayList<Integer>();
-		subPermute(num, visited, path, result);
-		return result;
-	}
+    public ArrayList<ArrayList<Integer>> permute(int[] num) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        boolean[] visited = new boolean[num.length];
 
-	private void subPermute(int[] num, boolean[] visited, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> result) {
-		if (path.size() == num.length) {
-			result.add(new ArrayList<Integer>(path));
-		} else {
-			for (int i = 0; i < num.length; i++) {
-				if (visited[i] == false) {
-					path.add(num[i]);
-					visited[i] = true;
-					subPermute(num, visited, path, result);
-					visited[i] = false;
-					path.remove(path.size() - 1);
-				}
-			}
-		}
-	}
+        doPermute(num, visited, path, result);
+        return result;
+    }
+
+    private void doPermute(int[] num, boolean[] visited, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> result) {
+        if (path.size() == num.length)
+            result.add(new ArrayList<Integer>(path));
+
+        for (int i = 0; i < num.length; i++) {
+            if (!visited[i]) {
+                path.add(num[i]);
+                visited[i] = true;
+                doPermute(num, visited, path, result);
+                path.remove(path.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
 }
