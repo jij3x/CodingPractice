4cd0516b1c0427ec7c4b14b2babde76d74682ad7
jij3x <jijex@yahoo.com>
2014-04-15 00:57:45 -0700

    Update Edit Distance.java

--- Edit Distance/Edit Distance.java
+++ Edit Distance/Edit Distance.java
@@ -29,3 +29,23 @@ public class Solution {
         return memo[i][j];
     }
 }
+
+class Solution2 {
+    public int minDistance(String word1, String word2) {
+        int[][] memo = new int[word1.length() + 1][word2.length() + 1];
+        for (int i = 0; i <= word1.length(); i++)
+            memo[i][word2.length()] = word1.length() - i;
+        for (int j = 0; j <= word2.length(); j++)
+            memo[word1.length()][j] = word2.length() - j;
+
+        for (int i = word1.length() - 1; i >= 0; i--) {
+            for (int j = word2.length() - 1; j >= 0; j--) {
+                if (word1.charAt(i) == word2.charAt(j))
+                    memo[i][j] = memo[i + 1][j + 1];
+                else
+                    memo[i][j] = Math.min(memo[i + 1][j], Math.min(memo[i][j + 1], memo[i + 1][j + 1])) + 1;
+            }
+        }
+        return memo[0][0];
+    }
+}
