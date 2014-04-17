b9746450cf53769ad9153a43c74c49a0a4438f12
jij3x <jijex@yahoo.com>
2014-04-17 11:25:08 -0700

    Update Best Time to Buy and Sell Stock II.java

--- Best Time to Buy and Sell Stock II/Best Time to Buy and Sell Stock II.java
+++ Best Time to Buy and Sell Stock II/Best Time to Buy and Sell Stock II.java
@@ -1,13 +1,12 @@
 public class Solution {
-	public int maxProfit(int[] prices) {
-		if (prices.length == 0)
-			return 0;
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0)
+            return 0;
 
-		int maxProfit = 0;
-		for (int i = 1; i < prices.length; i++) {
-			maxProfit += prices[i] > prices[i - 1] ? prices[i] - prices[i - 1] : 0;
-		}
-
-		return maxProfit;
-	}
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            max += prices[i] > prices[i - 1] ? prices[i] - prices[i - 1] : 0;
+        }
+        return max;
+    }
 }
