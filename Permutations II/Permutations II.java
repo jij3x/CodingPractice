c7c8e9f98b8572a119551fc83f62441d9d87ee66
jij3x <jijex@yahoo.com>
2014-04-18 22:03:18 -0700

    Update Permutations II.java

--- Permutations II/Permutations II.java
+++ Permutations II/Permutations II.java
@@ -1,35 +1,30 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
-		Arrays.sort(num);
-		boolean[] visited = new boolean[num.length];
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		ArrayList<Integer> path = new ArrayList<Integer>();
+    public ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+        Arrays.sort(num);
 
-		dfs(num, path, result, visited);
-		return result;
-	}
+        boolean[] visited = new boolean[num.length];
+        ArrayList<Integer> path = new ArrayList<Integer>();
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        doPermuteUnique(num, visited, path, result);
+        return result;
+    }
 
-	private void dfs(int[] num, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> result, boolean[] visited) {
-		if (path.size() == num.length) {
-			ArrayList<Integer> subResult = new ArrayList<Integer>(path);
-			result.add(subResult);
-		}
+    private void doPermuteUnique(int[] num, boolean[] visited, ArrayList<Integer> path,
+            ArrayList<ArrayList<Integer>> result) {
+        if (path.size() == num.length)
+            result.add(new ArrayList<Integer>(path));
 
-		for (int i = 0; i < num.length; i++) {
-			if (visited[i])
-				continue;
+        for (int i = 0; i < num.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                path.add(num[i]);
+                doPermuteUnique(num, visited, path, result);
+                path.remove(path.size() - 1);
+                visited[i] = false;
 
-			visited[i] = true;
-			path.add(num[i]);
-
-			dfs(num, path, result, visited);
-
-			visited[i] = false;
-			path.remove(path.size() - 1);
-
-			while (i <= num.length - 2 && num[i] == num[i + 1]) {
-				i++;
-			}
-		}
-	}
+                while (i < num.length - 1 && num[i] == num[i + 1])
+                    i++;
+            }
+        }
+    }
 }
