6a9a185dd76d133925c54fbc6310f7617fc9b282
jij3x <jijex@yahoo.com>
2014-04-19 06:54:08 -0700

    Update 4Sum.java

--- 4Sum/4Sum.java
+++ 4Sum/4Sum.java
@@ -1,39 +1,34 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
-		Arrays.sort(num);
+    public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+        Arrays.sort(num);
 
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		for (int i = 0; i <= num.length - 4; i++) {
-			if (i != 0 && num[i] == num[i - 1])
-				continue;
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i <= num.length - 4; i++) {
+            if (i > 0 && num[i] == num[i - 1])
+                continue;
 
-			for (int j = i + 1; j <= num.length - 3; j++) {
-				if (j != i + 1 && num[j] == num[j - 1])
-					continue;
+            for (int j = i + 1; j <= num.length - 3; j++) {
+                if (j > i + 1 && num[j] == num[j - 1])
+                    continue;
 
-				int head = j + 1;
-				int end = num.length - 1;
-				while (head < end) {
-					if (num[i] + num[j] + num[head] + num[end] == target) {
-						ArrayList<Integer> subResult = new ArrayList<Integer>();
-						subResult.add(num[i]);
-						subResult.add(num[j]);
-						subResult.add(num[head++]);
-						subResult.add(num[end--]);
-						result.add(subResult);
+                int start = j + 1, end = num.length - 1;
+                while (start < end) {
+                    if (num[i] + num[j] + num[start] + num[end] == target) {
+                        Integer[] r = { num[i], num[j], num[start++], num[end--] };
+                        result.add(new ArrayList<Integer>(Arrays.asList(r)));
 
-						while (head < end && num[head] == num[head - 1])
-							head++;
-						while (head < end && num[end] == num[end + 1])
-							end--;
-					} else if (num[i] + num[j] + num[head] + num[end] < target) {
-						head++;
-					} else {
-						end--;
-					}
-				}
-			}
-		}
-		return result;
-	}
+                        while (start < end && num[start] == num[start - 1])
+                            start++;
+                        while (start < end && num[end] == num[end + 1])
+                            end--;
+                    } else if (num[i] + num[j] + num[start] + num[end] > target) {
+                        end--;
+                    } else {
+                        start++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
