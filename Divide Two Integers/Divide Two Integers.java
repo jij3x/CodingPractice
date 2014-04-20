03460a59c139c7003c058cf1040234a48db83a68
jij3x <jijex@yahoo.com>
2014-04-19 20:34:21 -0700

    Update Divide Two Integers.java

--- Divide Two Integers/Divide Two Integers.java
+++ Divide Two Integers/Divide Two Integers.java
@@ -1,24 +1,22 @@
 public class Solution {
-	public int divide(int dividend, int divisor) {
-		long a = Math.abs((long) dividend);
-		long b = Math.abs((long) divisor);
+    public int divide(int dividend, int divisor) {
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor), bbk = b;
 
-		int result = 0;
-		int factor = 1;
-		long bb = b;
-		while (a >= b) {
-			if (a < bb) {
-				bb >>= 1;
-				factor >>= 1;
-			} else if (a >= bb << 1) {
-				bb <<= 1;
-				factor <<= 1;
-			} else {
-				result += factor;
-				a -= bb;
-			}
-		}
+        int result = 0, factor = 1;
+        while (a >= bbk) {
+            if (a < b) {
+                b >>= 1;
+                factor >>= 1;
+            } else if (a >= b << 1) {
+                b <<= 1;
+                factor <<= 1;
+            } else {
+                result += factor;
+                a -= b;
+            }
+        }
 
-		return (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0) ? result : -result;
-	}
+        return (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0) ? result : -result;
+    }
 }
