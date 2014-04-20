2cabd07dc48e7c1fd0d843a0dae9e726f48dfb2d
jij3x <jijex@yahoo.com>
2014-04-19 22:29:13 -0700

    Update Word Break.java

--- Word Break/Word Break.java
+++ Word Break/Word Break.java
@@ -1,39 +1,39 @@
 public class Solution {
-	public boolean wordBreak(String s, Set<String> dict) {
-		boolean[] memo = new boolean[s.length() + 1];
-		memo[0] = true;
-		for (int i = 1; i <= s.length(); i++) {
-			for (int j = i; j >= 0; j--) {
-				if (memo[j] && dict.contains(s.subSequence(j, i))) {
-					memo[i] = true;
-					break;
-				}
-			}
-		}
-		return memo[s.length()];
-	}
+    public boolean wordBreak(String s, Set<String> dict) {
+        byte[] memo = new byte[s.length()];
+        return dpWordBreak(s, dict, 0, memo);
+    }
+
+    private boolean dpWordBreak(String s, Set<String> dict, int start, byte[] memo) {
+        if (start >= s.length())
+            return true;
+        if (memo[start] != 0)
+            return memo[start] == 1 ? true : false;
+
+        for (int i = start; i < s.length(); i++) {
+            if (dict.contains(s.substring(start, i + 1)) && dpWordBreak(s, dict, i + 1, memo)) {
+                memo[start] = 1;
+                return true;
+            }
+        }
+
+        memo[start] = -1;
+        return false;
+    }
 }
 
 class Solution2 {
-	public boolean wordBreak(String s, Set<String> dict) {
-		byte[] memo = new byte[s.length()];
-		return doWordBreak(s, 0, dict, memo);
-	}
-
-	private boolean doWordBreak(String s, int start, Set<String> dict, byte[] memo) {
-		if (start >= s.length())
-			return true;
-		if (memo[start] != 0)
-			return memo[start] == 1 ? true : false;
-
-		for (int i = start; i < s.length(); i++) {
-			if (dict.contains(s.substring(start, i + 1)) && doWordBreak(s, i + 1, dict, memo)) {
-				memo[start] = 1;
-				return true;
-			}
-		}
-
-		memo[start] = -1;
-		return false;
-	}
+    public boolean wordBreak(String s, Set<String> dict) {
+        boolean[] memo = new boolean[s.length() + 1];
+        memo[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (memo[j] && dict.contains(s.substring(j, i))) {
+                    memo[i] = true;
+                    break;
+                }
+            }
+        }
+        return memo[s.length()];
+    }
 }
