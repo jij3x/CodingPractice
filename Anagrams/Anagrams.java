58875b010e8ad44d6ced329706f72b4cf3e7e8f3
jij3x <jijex@yahoo.com>
2014-04-20 09:32:14 -0700

    Update Anagrams.java

--- Anagrams/Anagrams.java
+++ Anagrams/Anagrams.java
@@ -1,22 +1,21 @@
 public class Solution {
-	public ArrayList<String> anagrams(String[] strs) {
-		HashMap<String, ArrayList<String>> wordMap = new HashMap<String, ArrayList<String>>();
-		for (int i = 0; i < strs.length; i++) {
-			char[] chars = strs[i].toCharArray();
-			Arrays.sort(chars);
-			String sortedWord = new String(chars);
+    public ArrayList<String> anagrams(String[] strs) {
+        HashMap<String, ArrayList<String>> wordMap = new HashMap<String, ArrayList<String>>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
 
-			ArrayList<String> anagrams = wordMap.containsKey(sortedWord) ? wordMap.get(sortedWord)
-					: new ArrayList<String>();
-			anagrams.add(strs[i]);
-			wordMap.put(sortedWord, anagrams);
-		}
+            ArrayList<String> anagrams = wordMap.containsKey(key) ? wordMap.get(key) : new ArrayList<String>();
+            anagrams.add(strs[i]);
+            wordMap.put(key, anagrams);
+        }
 
-		ArrayList<String> result = new ArrayList<String>();
-		for (ArrayList<String> anagrams : wordMap.values()) {
-			if (anagrams.size() > 1)
-				result.addAll(anagrams);
-		}
-		return result;
-	}
+        ArrayList<String> result = new ArrayList<String>();
+        for (ArrayList<String> anagrams : wordMap.values()) {
+            if (anagrams.size() > 1)
+                result.addAll(anagrams);
+        }
+        return result;
+    }
 }
