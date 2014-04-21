29e97fb40a1e104397e6e45fb63a4de781f2395e
jij3x <jijex@yahoo.com>
2014-04-20 17:28:59 -0700

    Update Binary Tree Zigzag Level Order Traversal.java

--- Binary Tree Zigzag Level Order Traversal/Binary Tree Zigzag Level Order Traversal.java
+++ Binary Tree Zigzag Level Order Traversal/Binary Tree Zigzag Level Order Traversal.java
@@ -8,41 +8,38 @@
  * }
  */
 public class Solution {
-	public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
-		ArrayList<ArrayList<Integer>> output = new ArrayList<ArrayList<Integer>>();
-		if (root == null)
-			return output;
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        if (root == null)
+            return result;
 
-		Stack<TreeNode> level1 = new Stack<TreeNode>();
-		Stack<TreeNode> level2 = new Stack<TreeNode>();
-		Stack<TreeNode> currentLevel = level1, nextLevel = level2;
+        Stack<TreeNode> stack1 = new Stack<TreeNode>(), stack2 = new Stack<TreeNode>();
+        Stack<TreeNode> curr = stack1, next = stack2;
+        curr.push(root);
+        ArrayList<Integer> row = new ArrayList<Integer>();
+        while (!curr.isEmpty()) {
+            TreeNode n = curr.pop();
+            row.add(n.val);
+            
+            if (curr == stack1) {
+                if (n.left != null)
+                    next.push(n.left);
+                if (n.right != null)
+                    next.push(n.right);
+            } else {
+                if (n.right != null)
+                    next.push(n.right);
+                if (n.left != null)
+                    next.push(n.left);
+            }
 
-		ArrayList<Integer> lineOutput = new ArrayList<Integer>();
-		currentLevel.push(root);
-		while (!currentLevel.empty()) {
-			TreeNode node = currentLevel.pop();
-			lineOutput.add(node.val);
-
-			if (currentLevel == level1) { // go from left to right
-				if (node.left != null)
-					nextLevel.push(node.left);
-				if (node.right != null)
-					nextLevel.push(node.right);
-			} else { // go from right to left
-				if (node.right != null)
-					nextLevel.push(node.right);
-				if (node.left != null)
-					nextLevel.push(node.left);
-			}
-
-			if (currentLevel.empty()) {
-				output.add(lineOutput);
-				lineOutput = new ArrayList<Integer>();
-
-				currentLevel = currentLevel == level1 ? level2 : level1;
-				nextLevel = nextLevel == level1 ? level2 : level1;
-			}
-		}
-		return output;
-	}
+            if (curr.isEmpty()) {
+                curr = curr == stack1 ? stack2 : stack1;
+                next = next == stack1 ? stack2 : stack1;
+                result.add(row);
+                row = new ArrayList<Integer>();
+            }
+        }
+        return result;
+    }
 }
