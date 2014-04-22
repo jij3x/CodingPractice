aa48319baba6adfab93f125d8505a6b5d22f0587
jij3x <jijex@yahoo.com>
2014-04-21 22:59:54 -0700

    Update Sort Colors.java

--- Sort Colors/Sort Colors.java
+++ Sort Colors/Sort Colors.java
@@ -1,21 +1,16 @@
 public class Solution {
-	public void sortColors(int[] A) {
-		int redAfter = 0;
-		int blueBefore = A.length - 1;
-		int i = 0;
-		while (i <= blueBefore) {
-			if (A[i] == 0) {
-				A[i] = A[redAfter];
-				A[redAfter] = 0;
-				redAfter++;
-				i++;
-			} else if (A[i] == 2) {
-				A[i] = A[blueBefore];
-				A[blueBefore] = 2;
-				blueBefore--;
-			} else {
-				i++;
-			}
-		}
-	}
+    public void sortColors(int[] A) {
+        int redAfter = 0, blueBefore = A.length - 1, whitePtr = 0;
+        while (whitePtr <= blueBefore) {
+            if (A[whitePtr] == 2) {
+                A[whitePtr] = A[blueBefore];
+                A[blueBefore--] = 2;
+            } else if (A[whitePtr] == 0) {
+                A[whitePtr++] = A[redAfter];
+                A[redAfter++] = 0;
+            } else {
+                whitePtr++;
+            }
+        }
+    }
 }
