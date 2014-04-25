6d1f1b3a959b02f334a565642c872f36579604ce
jij3x <jijex@yahoo.com>
2014-04-24 22:49:38 -0700

    Update Longest Palindromic Substring.java

--- Longest Palindromic Substring/Longest Palindromic Substring.java
+++ Longest Palindromic Substring/Longest Palindromic Substring.java
@@ -28,16 +28,18 @@ public class Solution {
 class Solution2 {
     public String longestPalindrome(String s) {
         boolean[][] memo = new boolean[s.length()][s.length()];
-        String result = "";
+        int maxLen = 0, maxPos = 0;
         for (int i = 0; i < s.length(); i++) {
             for (int j = i; j >= 0; j--) {
                 if (s.charAt(i) == s.charAt(j) && (i - j <= 1 || memo[j + 1][i - 1])) {
-                    if (i - j + 1 > result.length())
-                        result = s.substring(j, i + 1);
                     memo[j][i] = true;
+                    if (i - j + 1 > maxLen) {
+                        maxLen = i - j + 1;
+                        maxPos = j;
+                    }
                 }
             }
         }
-        return result;
+        return s.substring(maxPos, maxPos + maxLen);
     }
 }
