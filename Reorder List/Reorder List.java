aa6a53420a288ddac6fec14010fe7d70cb44b464
jij3x <jijex@yahoo.com>
2014-04-27 20:55:22 -0700

    Update Reorder List.java

--- Reorder List/Reorder List.java
+++ Reorder List/Reorder List.java
@@ -10,35 +10,33 @@
  * }
  */
 public class Solution {
-	public void reorderList(ListNode head) {
-		if (head == null || head.next == null)
-			return;
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null)
+            return;
 
-		ListNode prev = head;
-		ListNode head2 = head;
-		while (head2.next != null && head2.next.next != null) {
-			head2 = head2.next.next;
-			prev = prev.next;
-		}
-		head2 = prev.next;
-		prev.next = null;
+        ListNode prev = head, head2 = head;
+        while (head2.next != null && head2.next.next != null) {
+            head2 = head2.next.next;
+            prev = prev.next;
+        }
+        head2 = prev.next;
+        prev.next = null;
 
-		prev = null;
-		while (head2 != null) {
-			ListNode temp = head2;
-			head2 = head2.next;
-			temp.next = prev;
-			prev = temp;
-		}
-		head2 = prev;
-
-		prev = head;
-		while (head2 != null) {
-			ListNode temp = head2;
-			head2 = head2.next;
-			temp.next = prev.next;
-			prev.next = temp;
-			prev = prev.next.next;
-		}
-	}
+        prev = null;
+        while (head2 != null) {
+            ListNode temp = head2;
+            head2 = head2.next;
+            temp.next = prev;
+            prev = temp;
+        }
+        head2 = prev;
+        
+        while (head2 != null) {
+            ListNode temp = head2;
+            head2 = head2.next;
+            temp.next = head.next;
+            head.next = temp;
+            head = temp.next;
+        }
+    }
 }
