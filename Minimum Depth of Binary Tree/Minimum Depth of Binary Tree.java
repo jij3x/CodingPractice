4d0bb2dc08614212a62381ab0e0c6099e8a8f527
jij3x <jijex@yahoo.com>
2014-04-28 22:09:46 -0700

    Update Minimum Depth of Binary Tree.java

--- Minimum Depth of Binary Tree/Minimum Depth of Binary Tree.java
+++ Minimum Depth of Binary Tree/Minimum Depth of Binary Tree.java
@@ -8,26 +8,26 @@
  * }
  */
 public class Solution {
-	private int minDepth;
+    private int min;
 
-	public int minDepth(TreeNode root) {
-		if (root == null)
-			return 0;
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
 
-		minDepth = Integer.MAX_VALUE;
-		dfs(root, 0);
-		return minDepth;
-	}
+        min = Integer.MAX_VALUE;
+        dfs(root, 1);
+        return min;
+    }
 
-	private void dfs(TreeNode root, int depth) {
-		if (root.left == null && root.right == null) {
-			minDepth = Math.min(minDepth, depth + 1);
-			return;
-		}
+    private void dfs(TreeNode root, int depth) {
+        if (root.left == null && root.right == null) {
+            min = Math.min(min, depth);
+            return;
+        }
 
-		if (root.left != null)
-			dfs(root.left, depth + 1);
-		if (root.right != null)
-			dfs(root.right, depth + 1);
-	}
+        if (root.left != null)
+            dfs(root.left, depth + 1);
+        if (root.right != null)
+            dfs(root.right, depth + 1);
+    }
 }
