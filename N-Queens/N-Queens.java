80fe7b78dd6a84c235fef35cee020edb1b4ef038
jij3x <jijex@yahoo.com>
2014-05-01 22:14:08 -0700

    Update N-Queens.java

--- N-Queens/N-Queens.java
+++ N-Queens/N-Queens.java
@@ -1,37 +1,37 @@
 public class Solution {
-	public ArrayList<String[]> solveNQueens(int n) {
-		ArrayList<String[]> result = new ArrayList<String[]>();
-		int[] board = new int[n];
-		doSolveNQueens(n, 0, board, result);
-		return result;
-	}
+    public ArrayList<String[]> solveNQueens(int n) {
+        ArrayList<String[]> result = new ArrayList<String[]>();
+        int[] board = new int[n];
+        solve(board, 0, result);
+        return result;
+    }
 
-	private void doSolveNQueens(int n, int level, int[] board, ArrayList<String[]> result) {
-		if (level == n) {
-			String[] subResult = new String[n];
-			for (int i = 0; i < n; i++) {
-				String line = "";
-				for (int j = 0; j < n; j++)
-					line += j == board[i] ? 'Q' : '.';
-				subResult[i] = line;
-			}
-			result.add(subResult);
-			return;
-		}
+    private void solve(int[] board, int row, ArrayList<String[]> result) {
+        if (row == board.length) {
+            String[] subResult = new String[board.length];
+            char[] template = new char[board.length];
+            Arrays.fill(template, '.');
+            for (int i = 0; i < board.length; i++) {
+                template[board[i]] = 'Q';
+                subResult[i] = new String(template);
+                template[board[i]] = '.';
+            }
+            result.add(subResult);
+        }
 
-		for (int i = 0; i < n; i++) {
-			boolean validPosition = true;
-			for (int j = level - 1; j >= 0; j--) {
-				if (board[j] == i || board[j] == i - (level - j) || board[j] == i + (level - j)) {
-					validPosition = false;
-					break;
-				}
-			}
+        for (int i = 0; i < board.length; i++) {
+            boolean valid = true;
+            for (int j = row - 1; j >= 0; j--) {
+                if (board[j] == i || board[j] == i - (row - j) || board[j] == i + (row - j)) {
+                    valid = false;
+                    break;
+                }
+            }
 
-			if (validPosition) {
-				board[level] = i;
-				doSolveNQueens(n, level + 1, board, result);
-			}
-		}
-	}
+            if (valid) {
+                board[row] = i;
+                solve(board, row + 1, result);
+            }
+        }
+    }
 }
