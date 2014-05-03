12141d92087fd416a1e78abc87549a27eb40abbb
jij3x <jijex@yahoo.com>
2014-05-02 17:48:53 -0700

    Update Letter Combinations of a Phone Number.java

--- Letter Combinations of a Phone Number/Letter Combinations of a Phone Number.java
+++ Letter Combinations of a Phone Number/Letter Combinations of a Phone Number.java
@@ -1,19 +1,40 @@
 public class Solution {
-	public ArrayList<String> letterCombinations(String digits) {
-		String[] keypad = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    private final String[] charTbl = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
 
-		ArrayList<String> result = new ArrayList<String>();
-		if (digits.length() == 0) {
-			result.add("");
-			return result;
-		}
+    public ArrayList<String> letterCombinations(String digits) {
+        return doCombination(digits, 0);
+    }
 
-		String letters = keypad[(int) digits.charAt(0) - (int) '0'];
-		for (int i = 0; i < letters.length(); i++) {
-			ArrayList<String> subResult = letterCombinations(digits.substring(1));
-			for (String s : subResult)
-				result.add(letters.charAt(i) + s);
-		}
-		return result;
-	}
+    private ArrayList<String> doCombination(String digits, int start) {
+        ArrayList<String> result = new ArrayList<String>();
+        if (start == digits.length()) {
+            result.add("");
+            return result;
+        }
+
+        for (int i = 0; i < charTbl[digits.charAt(start) - '0'].length(); i++) {
+            for (String s : doCombination(digits, start + 1))
+                result.add(charTbl[digits.charAt(start) - '0'].charAt(i) + s);
+        }
+        return result;
+    }
+}
+
+class Solution2 {
+    private final String[] keypad = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+    public ArrayList<String> letterCombinations(String digits) {
+        ArrayList<String> result = new ArrayList<String>();
+        if (digits.length() == 0) {
+            result.add("");
+            return result;
+        }
+
+        String letters = keypad[(int) digits.charAt(0) - '0'];
+        for (int i = 0; i < letters.length(); i++) {
+            for (String s : letterCombinations(digits.substring(1)))
+                result.add(letters.charAt(i) + s);
+        }
+        return result;
+    }
 }
