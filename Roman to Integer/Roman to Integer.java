e98fa43c383925507880cbc04c2902efa4c8c6d5
jij3x <jijex@yahoo.com>
2014-05-04 11:22:17 -0700

    Update Roman to Integer.java

--- Roman to Integer/Roman to Integer.java
+++ Roman to Integer/Roman to Integer.java
@@ -1,21 +1,18 @@
 public class Solution {
-	public int romanToInt(String s) {
-		HashMap<Character, Integer> romanMap = new HashMap<Character, Integer>();
-		romanMap.put('I', 1);
-		romanMap.put('V', 5);
-		romanMap.put('X', 10);
-		romanMap.put('L', 50);
-		romanMap.put('C', 100);
-		romanMap.put('D', 500);
-		romanMap.put('M', 1000);
+    public int romanToInt(String s) {
+        int[] charMap = new int[256];
+        charMap[(int) 'I'] = 1;
+        charMap[(int) 'V'] = 5;
+        charMap[(int) 'X'] = 10;
+        charMap[(int) 'L'] = 50;
+        charMap[(int) 'C'] = 100;
+        charMap[(int) 'D'] = 500;
+        charMap[(int) 'M'] = 1000;
 
-		int result = romanMap.get(s.charAt(s.length() - 1));
-		for (int i = s.length() - 2; i >= 0; i--) {
-			if (romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i + 1)))
-				result -= romanMap.get(s.charAt(i));
-			else
-				result += romanMap.get(s.charAt(i));
-		}
-		return result;
-	}
+        int result = charMap[s.charAt(s.length() - 1)];
+        for (int i = s.length() - 2; i >= 0; i--)
+            result += (charMap[s.charAt(i)] < charMap[s.charAt(i + 1)] ? -1 : 1) * charMap[s.charAt(i)];
+
+        return result;
+    }
 }
