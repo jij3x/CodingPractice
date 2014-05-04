58e89c044039ec0fbd98759358b788d6a17cc4cf
jij3x <jijex@yahoo.com>
2014-05-04 11:23:29 -0700

    Update Roman to Integer.java

--- Roman to Integer/Roman to Integer.java
+++ Roman to Integer/Roman to Integer.java
@@ -1,13 +1,13 @@
 public class Solution {
     public int romanToInt(String s) {
         int[] charMap = new int[256];
-        charMap[(int) 'I'] = 1;
-        charMap[(int) 'V'] = 5;
-        charMap[(int) 'X'] = 10;
-        charMap[(int) 'L'] = 50;
-        charMap[(int) 'C'] = 100;
-        charMap[(int) 'D'] = 500;
-        charMap[(int) 'M'] = 1000;
+        charMap['I'] = 1;
+        charMap['V'] = 5;
+        charMap['X'] = 10;
+        charMap['L'] = 50;
+        charMap['C'] = 100;
+        charMap['D'] = 500;
+        charMap['M'] = 1000;
 
         int result = charMap[s.charAt(s.length() - 1)];
         for (int i = s.length() - 2; i >= 0; i--)
