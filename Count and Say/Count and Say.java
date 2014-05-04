837256377f315dadda9b31664e04a205d9778396
jij3x <jijex@yahoo.com>
2014-05-04 16:59:41 -0700

    Update Count and Say.java

--- Count and Say/Count and Say.java
+++ Count and Say/Count and Say.java
@@ -1,45 +1,42 @@
 public class Solution {
-	public String countAndSay(int n) {
-		String result = "1";
-		for (int i = 1; i < n; i++) {
-			result = convert(result);
-		}
-		return result;
-	}
+    public String countAndSay(int n) {
+        StringBuilder result = new StringBuilder("1");
+        for (int i = 1; i < n; i++) {
+            StringBuilder r = new StringBuilder();
+            for (int j = 0, count = 1; j < result.length(); j++, count++) {
+                if (j == result.length() - 1 || result.charAt(j) != result.charAt(j + 1)) {
+                    r.append(count).append(result.charAt(j));
+                    count = 0;
+                }
+            }
+            result = r;
+        }
 
-	private String convert(String input) {
-		String output = "";
-		for (int i = 0; i < input.length();) {
-			int count = 0, j;
-			for (j = i; j < input.length(); j++) {
-				if (input.charAt(j) == input.charAt(i))
-					count++;
-				else
-					break;
-			}
-			output += Integer.toString(count) + input.charAt(i);
-			i = j;
-		}
-		return output;
-	}
+        return result.toString();
+    }
 }
 
 class Solution2 {
-	public String countAndSay(int n) {
-		String result = "1";
+    public String countAndSay(int n) {
+        String result = "1";
+        for (int i = 1; i < n; i++) {
+            result = convert(result);
+        }
+        return result;
+    }
 
-		for (int i = 1; i < n; i++) {
-			String newString = "";
-			for (int j = 0, c = 0; j < result.length(); j++) {
-				c++;
-				if (j == result.length() - 1 || result.charAt(j) != result.charAt(j + 1)) {
-					newString += Integer.toString(c) + result.charAt(j);
-					c = 0;
-				}
-			}
-			result = newString;
-		}
-
-		return result;
-	}
+    private String convert(String input) {
+        StringBuilder output = new StringBuilder();
+        for (int i = 0, j = 0, count = 0; i < input.length();) {
+            for (j = i, count = 0; j < input.length(); j++) {
+                if (input.charAt(j) == input.charAt(i))
+                    count++;
+                else
+                    break;
+            }
+            output.append(count).append(input.charAt(i));
+            i = j;
+        }
+        return output.toString();
+    }
 }
