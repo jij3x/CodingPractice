faeceea2f519af243c76c140892104810a8b48de
jij3x <jijex@yahoo.com>
2014-05-04 10:43:56 -0700

    Update Integer to Roman.java

--- Integer to Roman/Integer to Roman.java
+++ Integer to Roman/Integer to Roman.java
@@ -1,28 +1,26 @@
 public class Solution {
-	public String intToRoman(int num) {
-		String[] thousands = { "", "M", "MM", "MMM" };
-		String[] hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
-		String[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
-		String[] ones = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+    private String[] thousands = { "", "M", "MM", "MMM" };
+    private String[] hundreds = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+    private String[] tens = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+    private String[] ones = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
-		return thousands[num / 1000] + hundreds[(num / 100) % 10] + tens[(num / 10) % 10] + ones[num % 10];
-	}
+    public String intToRoman(int num) {
+        return thousands[num / 1000] + hundreds[num % 1000 / 100] + tens[num % 100 / 10] + ones[num % 10];
+    }
 }
 
 class Solution2 {
-	public String intToRoman(int num) {
-		String[] roman = { "M", "DM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-		int[] number = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    public String intToRoman(int num) {
+        String[] roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        int[] number = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
 
-		int pivot = 0;
-		String result = "";
-		while (num > 0) {
-			int c = num / number[pivot];
-			num -= c * number[pivot];
-			for (int i = 1; i <= c; i++)
-				result += roman[pivot];
-			pivot++;
-		}
-		return result;
-	}
+        String result = "";
+        for (int i = 0; num > 0; i++) {
+            int cnt = num / number[i];
+            num -= cnt * number[i];
+            for (int j = 1; j <= cnt; j++)
+                result += roman[i];
+        }
+        return result;
+    }
 }
