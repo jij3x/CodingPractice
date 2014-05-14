97fb16821d3cf87d482a1479c8a96f198b94b64c
jij3x <jijex@yahoo.com>
2014-05-13 22:02:45 -0700

    Update Linked List Cycle.java

--- Linked List Cycle/Linked List Cycle.java
+++ Linked List Cycle/Linked List Cycle.java
@@ -12,9 +12,7 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         for (ListNode pioneer = head; pioneer != null && pioneer.next != null && pioneer.next.next != null;) {
-            head = head.next;
-            pioneer = pioneer.next.next;
-            if (head == pioneer)
+            if ((head = head.next) == (pioneer = pioneer.next.next))
                 return true;
         }
         return false;
