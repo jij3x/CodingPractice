524d937460e586cec3b41db6ba0d5c7c19d9b517
jij3x <jijex@yahoo.com>
2014-05-17 06:40:13 -0700

    Update Two Sum.java

--- Two Sum/Two Sum.java
+++ Two Sum/Two Sum.java
@@ -1,15 +1,15 @@
 public class Solution {
-	public int[] twoSum(int[] numbers, int target) {
-		int[] result = new int[2];
-		HashMap<Integer, Integer> table = new HashMap<Integer, Integer>();
-		for (int i = 0; i < numbers.length; i++) {
-			if (table.containsKey(target - numbers[i])) {
-				result[0] = table.get(target - numbers[i]) + 1;
-				result[1] = i + 1;
-			} else {
-				table.put(numbers[i], i);
-			}
-		}
-		return result;
-	}
+    public int[] twoSum(int[] numbers, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> tbl = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (tbl.containsKey(target - numbers[i])) {
+                result[0] = tbl.get(target - numbers[i]) + 1;
+                result[1] = i + 1;
+                break;
+            }
+            tbl.put(numbers[i], i);
+        }
+        return result;
+    }
 }
