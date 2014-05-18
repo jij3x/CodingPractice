7507d75fcb83b50fba01f0680253ae24f297c12d
jij3x <jijex@yahoo.com>
2014-05-18 08:25:45 -0700

    Update Reverse Integer.java

--- Reverse Integer/Reverse Integer.java
+++ Reverse Integer/Reverse Integer.java
@@ -2,10 +2,9 @@ public class Solution {
     public int reverse(int x) {
         int result = 0;
         while (x != 0) {
-            result = result*10 + x%10;
+            result = result * 10 + x % 10;
             x /= 10;
         }
-        
         return result;
     }
 }
