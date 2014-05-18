959c3dd7613ff91ae8912e502262bc1cf2d3bb67
jij3x <jijex@yahoo.com>
2014-05-18 07:45:11 -0700

    Update Remove Duplicates from Sorted Array II.java

--- Remove Duplicates from Sorted Array II/Remove Duplicates from Sorted Array II.java
+++ Remove Duplicates from Sorted Array II/Remove Duplicates from Sorted Array II.java
@@ -4,7 +4,7 @@ public class Solution {
             return A.length;
 
         int len = 2;
-        for (int i = 2, penultimate = A[0]; i < A.length; i++) {
+        for (int i = 2; i < A.length; i++) {
             if (A[i] != A[len - 2])
                 A[len++] = A[i];
         }
