a4b2c30426fc537e40d3b8875519165b80b0c937
jij3x <jijex@yahoo.com>
2014-05-18 07:56:16 -0700

    Update Remove Duplicates from Sorted List.java

--- Remove Duplicates from Sorted List/Remove Duplicates from Sorted List.java
+++ Remove Duplicates from Sorted List/Remove Duplicates from Sorted List.java
@@ -11,13 +11,11 @@
  */
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        if (head != null) {
-            for (ListNode pivot = head; pivot.next != null;) {
-                if (pivot.val == pivot.next.val)
-                    pivot.next = pivot.next.next;
-                else
-                    pivot = pivot.next;
-            }
+        for (ListNode pivot = head; pivot != null && pivot.next != null;) {
+            if (pivot.val == pivot.next.val)
+                pivot.next = pivot.next.next;
+            else
+                pivot = pivot.next;
         }
         return head;
     }
