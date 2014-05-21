48038946ccd057bab20b254a717567d58180a75e
jij3x <jijex@yahoo.com>
2014-05-21 11:34:37 -0700

    Update Decode Ways.java

--- Decode Ways/Decode Ways.java
+++ Decode Ways/Decode Ways.java
@@ -1,47 +1,41 @@
 public class Solution {
-	public int numDecodings(String s) {
-		if (s.isEmpty())
-			return 0;
+    public int numDecodings(String s) {
+        if (s.isEmpty())
+            return 0;
 
-		int[] dp = new int[s.length() + 1];
-		dp[s.length()] = 1;
-		for (int i = s.length() - 1; i >= 0; i--) {
-			if (s.charAt(i) == '0')
-				dp[i] = 0;
-			else
-				dp[i] = i < s.length() - 1 && Integer.parseInt(s.substring(i, i + 2)) <= 26 ? dp[i + 1] + dp[i + 2]
-						: dp[i + 1];
-		}
+        int[] memo = new int[s.length() + 2];
+        memo[s.length()] = 1;
+        memo[s.length() - 1] = s.charAt(s.length() - 1) == '0' ? 0 : 1;
 
-		return dp[0];
-	}
+        for (int i = s.length() - 2; i >= 0; i--) {
+            if (s.charAt(i) != '0')
+                memo[i] += memo[i + 1] + (Integer.parseInt(s.substring(i, i + 2)) < 27 ? memo[i + 2] : 0);
+        }
+        return memo[0];
+    }
 }
 
 class Solution2 {
-	public int numDecodings(String s) {
-		if (s.isEmpty())
-			return 0;
+    public int numDecodings(String s) {
+        if (s.isEmpty())
+            return 0;
 
-		int[] memo = new int[s.length()];
-		Arrays.fill(memo, -1);
-		return dpNumDecodings(s, 0, memo);
+        int[] memo = new int[s.length() + 1];
+        Arrays.fill(memo, -1);
+        memo[s.length()] = 1;
+        return doDecode(s, 0, memo);
+    }
 
-	}
+    private int doDecode(String s, int start, int[] memo) {
+        if (memo[start] != -1)
+            return memo[start];
+        if (s.charAt(start) == '0')
+            return 0;
 
-	private int dpNumDecodings(String s, int si, int[] memo) {
-		if (si == s.length())
-			return 1;
-		else if (s.charAt(si) == '0')
-			return 0;
+        memo[start] = doDecode(s, start + 1, memo);
+        if (start < s.length() - 1 && Integer.parseInt(s.substring(start, start + 2)) < 27)
+            memo[start] += doDecode(s, start + 2, memo);
 
-		if (memo[si] != -1)
-			return memo[si];
-
-		int result = dpNumDecodings(s, si + 1, memo);
-		if (si < s.length() - 1 && Integer.parseInt(s.substring(si, si + 2)) <= 26)
-			result += dpNumDecodings(s, si + 2, memo);
-
-		memo[si] = result;
-		return result;
-	}
+        return memo[start];
+    }
 }
