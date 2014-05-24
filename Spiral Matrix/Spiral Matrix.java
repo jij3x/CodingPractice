6ba4a423e95afd3c8bae3b0608126a5608ee8cbb
jij3x <jijex@yahoo.com>
2014-05-23 20:47:12 -0700

    Update Spiral Matrix.java

--- Spiral Matrix/Spiral Matrix.java
+++ Spiral Matrix/Spiral Matrix.java
@@ -1,31 +1,31 @@
 public class Solution {
-	public ArrayList<Integer> spiralOrder(int[][] matrix) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		if (matrix.length == 0 || matrix[0].length == 0)
-			return result;
+    public ArrayList<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (matrix.length == 0 || matrix[0].length == 0)
+            return result;
 
-		int left = 0, top = 0, bottom = matrix.length - 1, right = matrix[0].length - 1;
-		while (true) {
-			for (int i = left; i <= right; i++)
-				result.add(matrix[top][i]);
-			if (++top > bottom)
-				break;
+        int top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
+        while (true) {
+            for (int i = left; i <= right; i++)
+                result.add(matrix[top][i]);
+            if (++top > bottom)
+                break;
 
-			for (int i = top; i <= bottom; i++)
-				result.add(matrix[i][right]);
-			if (--right < left)
-				break;
+            for (int i = top; i <= bottom; i++)
+                result.add(matrix[i][right]);
+            if (--right < left)
+                break;
 
-			for (int i = right; i >= left; i--)
-				result.add(matrix[bottom][i]);
-			if (--bottom < top)
-				break;
+            for (int i = right; i >= left; i--)
+                result.add(matrix[bottom][i]);
+            if (--bottom < top)
+                break;
 
-			for (int i = bottom; i >= top; i--)
-				result.add(matrix[i][left]);
-			if (++left > right)
-				break;
-		}
-		return result;
-	}
+            for (int i = bottom; i >= top; i--)
+                result.add(matrix[i][left]);
+            if (++left > right)
+                break;
+        }
+        return result;
+    }
 }
