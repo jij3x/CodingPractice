454ccf5575cb0a12bed84184b7577814d493bc72
jij3x <jijex@yahoo.com>
2014-05-25 07:54:52 -0700

    Update Subsets.java

--- Subsets/Subsets.java
+++ Subsets/Subsets.java
@@ -1,13 +1,13 @@
 public class Solution {
-    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    public List<List<Integer>> subsets(int[] S) {
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
         result.add(new ArrayList<Integer>());
 
         Arrays.sort(S);
         for (int i = 0; i < S.length; i++) {
             int len = result.size();
             for (int j = 0; j < len; j++) {
-                ArrayList<Integer> r = new ArrayList<Integer>(result.get(j));
+                List<Integer> r = new ArrayList<Integer>(result.get(j));
                 r.add(S[i]);
                 result.add(r);
             }
@@ -17,19 +17,19 @@ public class Solution {
 }
 
 class Solution2 {
-    public ArrayList<ArrayList<Integer>> subsets(int[] S) {
+    public List<List<Integer>> subsets(int[] S) {
         Arrays.sort(S);
-        return doSubset(S, 0);
+        return doSubsets(S, 0);
     }
 
-    private ArrayList<ArrayList<Integer>> doSubset(int[] S, int idx) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+    private List<List<Integer>> doSubsets(int[] S, int idx) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
         result.add(new ArrayList<Integer>());
         if (idx == S.length)
             return result;
 
         for (int i = idx; i < S.length; i++) {
-            for (ArrayList<Integer> r : doSubset(S, i + 1)) {
+            for (List<Integer> r : doSubsets(S, i + 1)) {
                 r.add(0, S[i]);
                 result.add(r);
             }
