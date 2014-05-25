50692d2bad19e4cb1c561302ca2cb0575018c5b9
jij3x <jijex@yahoo.com>
2014-05-25 08:36:47 -0700

    Update Word Search.java

--- Word Search/Word Search.java
+++ Word Search/Word Search.java
@@ -1,29 +1,29 @@
 public class Solution {
-	public boolean exist(char[][] board, String word) {
-		boolean[][] visited = new boolean[board.length][board[0].length];
-		for (int y = 0; y < board.length; y++) {
-			for (int x = 0; x < board[0].length; x++) {
-				if (dfs(board, y, x, word, 0, visited))
-					return true;
-			}
-		}
-		return false;
-	}
+    public boolean exist(char[][] board, String word) {
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for (int y = 0; y < board.length; y++) {
+            for (int x = 0; x < board[0].length; x++) {
+                if (dfs(board, y, x, word, 0, visited))
+                    return true;
+            }
+        }
+        return false;
+    }
 
-	private boolean dfs(char[][] board, int y, int x, String word, int s, boolean[][] visited) {
-		if (s == word.length())
-			return true;
+    private boolean dfs(char[][] board, int y, int x, String word, int s, boolean[][] visited) {
+        if (s == word.length())
+            return true;
 
-		if (y < 0 || y >= board.length || x < 0 || x >= board[0].length || visited[y][x]
-				|| board[y][x] != word.charAt(s))
-			return false;
+        if (y < 0 || y == board.length || x < 0 || x == board[0].length || visited[y][x]
+                || word.charAt(s) != board[y][x])
+            return false;
 
-		visited[y][x] = true;
-		if (dfs(board, y + 1, x, word, s + 1, visited) || dfs(board, y - 1, x, word, s + 1, visited)
-				|| dfs(board, y, x + 1, word, s + 1, visited) || dfs(board, y, x - 1, word, s + 1, visited))
-			return true;
+        visited[y][x] = true;
+        if (dfs(board, y - 1, x, word, s + 1, visited) || dfs(board, y + 1, x, word, s + 1, visited)
+                || dfs(board, y, x - 1, word, s + 1, visited) || dfs(board, y, x + 1, word, s + 1, visited))
+            return true;
 
-		visited[y][x] = false;
-		return false;
-	}
+        visited[y][x] = false;
+        return false;
+    }
 }
