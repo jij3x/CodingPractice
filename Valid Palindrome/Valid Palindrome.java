08c046b91747456e66901c35d7be313662297890
jij3x <jijex@yahoo.com>
2014-05-25 08:57:12 -0700

    Update Valid Palindrome.java

--- Valid Palindrome/Valid Palindrome.java
+++ Valid Palindrome/Valid Palindrome.java
@@ -1,21 +1,14 @@
 public class Solution {
-	public boolean isPalindrome(String s) {
-		s = s.toLowerCase();
-		int head = 0;
-		int tail = s.length() - 1;
-		while (head < tail) {
-			if ((Character.isLetter(s.charAt(head)) || Character.isDigit(s.charAt(head)))
-					&& (Character.isLetter(s.charAt(tail)) || Character.isDigit(s.charAt(tail)))) {
-				if (s.charAt(head) != s.charAt(tail))
-					return false;
-				head++;
-				tail--;
-			} else if (Character.isLetter(s.charAt(head)) || Character.isDigit(s.charAt(head))) {
-				tail--;
-			} else {
-				head++;
-			}
-		}
-		return true;
-	}
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        for (int start = 0, end = s.length() - 1; start < end;) {
+            if (!Character.isLetter(s.charAt(start)) && !Character.isDigit(s.charAt(start)))
+                start++;
+            else if (end >= 0 && !Character.isLetter(s.charAt(end)) && !Character.isDigit(s.charAt(end)))
+                end--;
+            else if (s.charAt(start++) != s.charAt(end--))
+                return false;
+        }
+        return true;
+    }
 }
