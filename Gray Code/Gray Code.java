488356634d6e4616b99e3a4d967c1b259b9be278
jij3x <jijex@yahoo.com>
2014-06-02 23:51:04 -0700

    Update Gray Code.java

--- Gray Code/Gray Code.java
+++ Gray Code/Gray Code.java
@@ -1,31 +1,26 @@
 public class Solution {
-	public ArrayList<Integer> grayCode(int n) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<Integer>();
+        if (n == 0) {
+            result.add(0);
+            return result;
+        }
 
-		for (int i = 0; i < (1 << n); i++)
-			result.add((i >> 1) ^ i);
+        result.addAll(grayCode(n - 1));
+        for (int i = result.size() - 1; i >= 0; i--)
+            result.add((1 << (n - 1)) + result.get(i));
 
-		return result;
-	}
+        return result;
+    }
 }
 
 class Solution2 {
-	public ArrayList<Integer> grayCode(int n) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		if (n == 0) {
-		    result.add(0);
-		} else if (n == 1) {
-			result.add(0);
-			result.add(1);
-		} else {
-			ArrayList<Integer> subResult = grayCode(n - 1);
-			result.addAll(subResult);
-			
-			for (int i = subResult.size() - 1; i >= 0; i--) {
-				result.add((1 << (n - 1)) + subResult.get(i));
-			}
-		}
+    public ArrayList<Integer> grayCode(int n) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
 
-		return result;
-	}
+        for (int i = 0; i < (1 << n); i++)
+            result.add((i >> 1) ^ i);
+
+        return result;
+    }
 }
