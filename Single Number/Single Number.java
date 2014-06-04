1562ecc0145e6835ecff78e73ef9fd29d81c4978
jij3x <jijex@yahoo.com>
2014-06-03 23:07:49 -0700

    Update Single Number.java

--- Single Number/Single Number.java
+++ Single Number/Single Number.java
@@ -1,9 +1,9 @@
 public class Solution {
-	public int singleNumber(int[] A) {
-		int result = 0;
-		for (int i = 0; i < A.length; i++) {
-			result ^= A[i];
-		}
-		return result;
-	}
+    public int singleNumber(int[] A) {
+        int result = 0;
+        for (int i = 0; i < A.length; i++) {
+            result ^= A[i];
+        }
+        return result;
+    }
 }
