1259e554f89f9de887b09bd33e539ab2cdac0eda
jij3x <jijex@yahoo.com>
2014-06-23 21:45:46 -0700

    Update Unique Binary Search Trees.java

--- Unique Binary Search Trees/Unique Binary Search Trees.java
+++ Unique Binary Search Trees/Unique Binary Search Trees.java
@@ -1,15 +1,11 @@
 public class Solution {
-	public int numTrees(int n) {
-		if (n == 0)
-			return 1;
+    public int numTrees(int n) {
+        if (n == 0)
+            return 1;
 
-		int total = 0;
-		for (int i = 0; i < n; i++) {
-			int leftTotal = numTrees(i);
-			int rightTotal = numTrees(n - 1 - i);
-			total += leftTotal * rightTotal;
-		}
-
-		return total;
-	}
+        int total = 0;
+        for (int i = 0; i < n; i++)
+            total += numTrees(i) * numTrees(n - 1 - i);
+        return total;
+    }
 }
