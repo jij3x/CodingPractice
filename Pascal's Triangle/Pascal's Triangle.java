9e0fed37d7099428558b286ae42843f15ac47aa7
jij3x <jijex@yahoo.com>
2014-06-24 16:49:46 -0700

    Update Pascal's Triangle.java

--- Pascal's Triangle/Pascal's Triangle.java
+++ Pascal's Triangle/Pascal's Triangle.java
@@ -1,21 +1,19 @@
 public class Solution {
-	public ArrayList<ArrayList<Integer>> generate(int numRows) {
-		ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-		if (numRows == 0)
-			return result;
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (numRows == 0)
+            return result;
 
-		for (int i = 0; i < numRows; i++) {
-			ArrayList<Integer> row = new ArrayList<Integer>();
-			for (int j = 0; j <= i; j++) {
-				if (j == 0 || j == i) {
-					row.add(1);
-				} else {
-					int thisNum = result.get(i - 1).get(j - 1) + result.get(i - 1).get(j);
-					row.add(thisNum);
-				}
-			}
-			result.add(row);
-		}
-		return result;
-	}
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i)
+                    row.add(1);
+                else
+                    row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+            }
+            result.add(row);
+        }
+        return result;
+    }
 }
