fa54dccd5d23365b9bd5ee7deab58edf3b756a0c
jij3x <jijex@yahoo.com>
2014-06-23 23:02:29 -0700

    Update Triangle.java

--- Triangle/Triangle.java
+++ Triangle/Triangle.java
@@ -1,12 +1,10 @@
 public class Solution {
-	public int minimumTotal(ArrayList<ArrayList<Integer>> triangle) {
-		int[] memo = new int[triangle.size() + 1];
-		for (int i = triangle.size() - 1; i >= 0; i--) {
-			for (int j = 0; j <= i; j++) {
-				memo[j] = Math.min(memo[j], memo[j + 1]) + triangle.get(i).get(j);
-			}
-		}
-
-		return memo[0];
-	}
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[] memo = new int[triangle.size() + 1];
+        for (int y = triangle.size() - 1; y >= 0; y--) {
+            for (int x = 0; x <= y; x++)
+                memo[x] = Math.min(memo[x], memo[x + 1]) + triangle.get(y).get(x);
+        }
+        return memo[0];
+    }
 }
