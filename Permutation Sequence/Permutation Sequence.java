30c2c2f449332d547d30dc8870d739fc04b8675f
jij3x <jijex@yahoo.com>
2014-06-25 22:00:06 -0700

    Update Permutation Sequence.java

--- Permutation Sequence/Permutation Sequence.java
+++ Permutation Sequence/Permutation Sequence.java
@@ -1,68 +1,52 @@
 public class Solution {
-	public String getPermutation(int n, int k) {
-		int[] digits = new int[n];
-		for (int i = 0; i < n; i++) {
-			digits[i] = i + 1;
-		}
+    public String getPermutation(int n, int k) {
+        int[] fTbl = new int[n + 1];
+        fTbl[0] = fTbl[1] = 1;
+        for (int i = 2; i <= n; i++)
+            fTbl[i] = i * fTbl[i - 1];
 
-		String result = "";
-		while (n >= 1) {
-			int idx = (k - 1) / factorial(n - 1);
-			int num = digits[idx];
-			for (int i = idx; i < n - 1; i++) {
-				digits[i] = digits[i + 1];
-			}
-
-			result += Integer.toString(num);
-			k = (k - 1) % factorial(n - 1) + 1;
-			n--;
-		}
-		return result;
-	}
-
-	private int factorial(int n) {
-		int result = 1;
-		for (int i = 1; i <= n; i++) {
-			result *= i;
-		}
-
-		return result;
-	}
+        boolean[] visited = new boolean[n + 1];
+        int result = 0;
+        while (n > 0) {
+            int seg = (k - 1) / fTbl[n - 1] + 1;
+            for (int i = 1; i < visited.length; i++) {
+                if (!visited[i] && --seg == 0) {
+                    visited[i] = true;
+                    result = result * 10 + i;
+                    break;
+                }
+            }
+            k = (k - 1) % fTbl[n - 1] + 1;
+            n--;
+        }
+        return Integer.toString(result);
+    }
 }
 
 class Solution2 {
-	public String getPermutation(int n, int k) {
-		int[] num = new int[n];
-		for (int i = 0; i < n; i++) {
-			num[i] = i + 1;
-		}
-		return subGetPermutation(num, n, k);
-	}
+    public String getPermutation(int n, int k) {
+        int[] fTbl = new int[n + 1];
+        fTbl[0] = fTbl[1] = 1;
+        for (int i = 2; i <= n; i++)
+            fTbl[i] = i * fTbl[i - 1];
 
-	private String subGetPermutation(int[] num, int length, int k) {
-		String result = "";
-		if (k == 1) {
-			for (int i = 0; i < length; i++)
-				result += Integer.toString(num[i]);
-			return result;
-		}
+        boolean[] visited = new boolean[n + 1];
+        return doGetPerm(n, k, fTbl, visited);
+    }
 
-		int index = (k - 1) / factorial(length - 1);
-		result = Integer.toString(num[index]);
-		for (int i = index; i < num.length - 1; i++) {
-			num[i] = num[i + 1];
-		}
+    private String doGetPerm(int n, int k, int[] fTbl, boolean[] visited) {
+        if (n == 0)
+            return "";
 
-		result += subGetPermutation(num, length - 1, (k - 1) % factorial(length - 1) + 1);
-		return result;
-	}
-
-	private int factorial(int n) {
-		int result = 1;
-		for (int i = 1; i <= n; i++) {
-			result *= i;
-		}
-
-		return result;
-	}
+        int seg = (k - 1) / fTbl[n - 1] + 1;
+        String num = "";
+        for (int i = 1; i < visited.length; i++) {
+            if (!visited[i] && --seg == 0) {
+                num = Integer.toString(i);
+                visited[i] = true;
+                break;
+            }
+        }
+        return num + doGetPerm(n - 1, (k - 1) % fTbl[n - 1] + 1, fTbl, visited);
+    }
 }
