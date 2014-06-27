70694c5a3085d90d5f13ec0525fdb1922e531d2d
jij3x <jijex@yahoo.com>
2014-06-26 21:56:35 -0700

    Update Add Two Numbers.java

--- Add Two Numbers/Add Two Numbers.java
+++ Add Two Numbers/Add Two Numbers.java
@@ -10,18 +10,18 @@
  * }
  */
 public class Solution {
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		int carry = 0;
-		ListNode start = new ListNode(0), tail = start;
-		while (l1 != null || l2 != null || carry != 0) {
-			carry += (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
-			tail.next = new ListNode(carry % 10);
-			tail = tail.next;
-			carry /= 10;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0;
+        ListNode start = new ListNode(0), tail = start;
+        while (l1 != null || l2 != null || carry != 0) {
+            carry += (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val);
+            tail.next = new ListNode(carry % 10);
+            tail = tail.next;
+            carry /= 10;
 
-			l1 = l1 == null ? null : l1.next;
-			l2 = l2 == null ? null : l2.next;
-		}
-		return start.next;
-	}
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        return start.next;
+    }
 }
