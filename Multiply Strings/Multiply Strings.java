5d5903d6c9cde3f6c5acaf17a0614fcee5957f24
jij3x <jijex@yahoo.com>
2014-06-26 22:55:21 -0700

    Update Multiply Strings.java

--- Multiply Strings/Multiply Strings.java
+++ Multiply Strings/Multiply Strings.java
@@ -1,25 +1,20 @@
 public class Solution {
-	public String multiply(String num1, String num2) {
-		int[] result = new int[num1.length() + num2.length()];
+    public String multiply(String num1, String num2) {
+        int[] buffer = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                buffer[i + j + 1] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                buffer[i + j] += buffer[i + j + 1] / 10;
+                buffer[i + j + 1] %= 10;
+            }
+        }
 
-		for (int i = num1.length() - 1; i >= 0; i--) {
-			int carry = 0;
-			for (int j = num2.length() - 1; j >= 0; j--) {
-				int product = ((int) num2.charAt(j) - (int) '0') * ((int) num1.charAt(i) - (int) '0');
-				product += carry + result[i + j + 1];
-				result[i + j + 1] = product % 10;
-				carry = product / 10;
-			}
-			result[result.length - num2.length() - 1 - (num1.length() - i - 1)] = carry;
-		}
-
-		int start = 0;
-		while (start < result.length - 1 && result[start] == 0)
-			start++;
-
-		String ret = "";
-		for (int i = start; i < result.length; i++)
-			ret += (char) (result[i] + (int) '0');
-		return ret;
-	}
+        StringBuilder result = new StringBuilder();
+        for (int i = 0, sp = 0; i < buffer.length; i++) {
+            sp += buffer[i] == 0 ? 0 : 1;
+            if (sp != 0)
+                result.append((char) (buffer[i] + '0'));
+        }
+        return result.length() == 0 ? "0" : result.toString();
+    }
 }
