579cb4936ce96463e3c3372d947c5c3d9c6c4a8d
jij3x <jijex@yahoo.com>
2014-06-28 10:18:12 -0700

    Update Valid Parentheses.java

--- Valid Parentheses/Valid Parentheses.java
+++ Valid Parentheses/Valid Parentheses.java
@@ -1,21 +1,16 @@
 public class Solution {
-	public boolean isValid(String s) {
-		Stack<Character> workingStack = new Stack<Character>();
-		for (int i = 0; i < s.length(); i++) {
-			if (s.charAt(i) == ')' && (workingStack.isEmpty() || workingStack.pop() != '(')) {
-				return false;
-			} else if (s.charAt(i) == ']' && (workingStack.isEmpty() || workingStack.pop() != '[')) {
-				return false;
-			} else if (s.charAt(i) == '}' && (workingStack.isEmpty() || workingStack.pop() != '{')) {
-				return false;
-			} else {
-				workingStack.push(s.charAt(i));
-			}
-		}
-
-		if (workingStack.size() > 0)
-			return false;
-
-		return true;
-	}
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{')
+                stack.push(s.charAt(i));
+            else if (s.charAt(i) == ')' && (stack.isEmpty() || stack.pop() != '('))
+                return false;
+            else if (s.charAt(i) == ']' && (stack.isEmpty() || stack.pop() != '['))
+                return false;
+            else if (s.charAt(i) == '}' && (stack.isEmpty() || stack.pop() != '{'))
+                return false;
+        }
+        return stack.size() == 0;
+    }
 }
