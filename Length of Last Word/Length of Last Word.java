cc29286a2ba939cca62df6e00860f7860419f348
jij3x <jijex@yahoo.com>
2014-06-28 15:41:20 -0700

    Update Length of Last Word.java

--- Length of Last Word/Length of Last Word.java
+++ Length of Last Word/Length of Last Word.java
@@ -1,21 +1,15 @@
 public class Solution {
-	public int lengthOfLastWord(String s) {
-		int i = s.length() - 1;
-		while (i >= 0) {
-			if (s.charAt(i) != ' ')
-				break;
-			i--;
-		}
+    public int lengthOfLastWord(String s) {
+        int i = s.length() - 1, length = 0;
+        while (i >= 0 && s.charAt(i) == ' ')
+            i--;
 
-		int ret = 0;
-		while (i >= 0) {
-			if (s.charAt(i) == ' ')
-				return ret;
-
-			ret++;
-			i--;
-		}
-
-		return ret;
-	}
+        while (i >= 0) {
+            if (s.charAt(i--) != ' ')
+                length++;
+            else
+                break;
+        }
+        return length;
+    }
 }
