da3d54780620c09a3134da859fb4c56e04bbb361
jij3x <jijex@yahoo.com>
2014-06-28 15:31:43 -0700

    Update Simplify Path.java

--- Simplify Path/Simplify Path.java
+++ Simplify Path/Simplify Path.java
@@ -1,47 +1,41 @@
 public class Solution {
-	public String simplifyPath(String path) {
-		Stack<StringBuilder> workingStack = new Stack<StringBuilder>();
-		for (int i = 0; i < path.length(); i++) {
-			if (path.charAt(i) == '/')
-				continue;
+    public String simplifyPath(String path) {
+        LinkedList<StringBuilder> buffer = new LinkedList<StringBuilder>();
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == '/')
+                continue;
 
-			StringBuilder directory = new StringBuilder("");
-			while (i < path.length() && path.charAt(i) != '/') {
-				directory.append(path.charAt(i++));
-			}
-			if (directory.toString().equals("..")) {
-				if (!workingStack.isEmpty())
-					workingStack.pop();
-			} else if (!directory.toString().equals(".")) {
-				workingStack.push(directory);
-			}
-		}
+            StringBuilder dir = new StringBuilder();
+            while (i < path.length() && path.charAt(i) != '/')
+                dir.append(path.charAt(i++));
 
-		StringBuilder result = new StringBuilder("");
-		while (!workingStack.isEmpty()) {
-			result.insert(0, '/').insert(1, workingStack.pop());
-		}
-		return result.length() == 0 ? "/" : result.toString();
-	}
+            if (!dir.toString().equals(".") && !dir.toString().equals(".."))
+                buffer.add(dir);
+            else if (dir.toString().equals("..") && !buffer.isEmpty())
+                buffer.removeLast();
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder s : buffer)
+            result.append('/').append(s);
+        return result.length() == 0 ? "/" : result.toString();
+    }
 }
 
 class Solution2 {
-	public String simplifyPath(String path) {
-		Stack<String> workingStack = new Stack<String>();
-		String[] directories = path.trim().split("/");
-		for (String d : directories) {
-			if (d.equals("..")) {
-				if (workingStack.size() > 0)
-					workingStack.pop();
-			} else if (!d.isEmpty() && !d.equals(".")) {
-				workingStack.push(d);
-			}
-		}
+    public String simplifyPath(String path) {
+        LinkedList<String> buffer = new LinkedList<String>();
+        String[] dirs = path.trim().split("/");
+        for (String d : dirs) {
+            if (!d.isEmpty() && !d.equals("..") && !d.equals("."))
+                buffer.add(d);
+            else if (d.equals("..") && !buffer.isEmpty())
+                buffer.removeLast();
+        }
 
-		StringBuilder result = new StringBuilder("");
-		while (!workingStack.isEmpty()) {
-			result.insert(0, '/').insert(1, workingStack.pop());
-		}
-		return result.length() == 0 ? "/" : result.toString();
-	}
+        StringBuilder result = new StringBuilder("");
+        for (String s : buffer)
+            result.append('/').append(s);
+        return result.length() == 0 ? "/" : result.toString();
+    }
 }
