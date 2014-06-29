d917219226f0fceeeb1a45646f561357651d0b43
jij3x <jijex@yahoo.com>
2014-06-29 15:52:24 -0700

    Update Construct Binary Tree from Inorder and Postorder Traversal.java

--- Construct Binary Tree from Inorder and Postorder Traversal/Construct Binary Tree from Inorder and Postorder Traversal.java
+++ Construct Binary Tree from Inorder and Postorder Traversal/Construct Binary Tree from Inorder and Postorder Traversal.java
@@ -8,25 +8,21 @@
  * }
  */
 public class Solution {
-	public TreeNode buildTree(int[] inorder, int[] postorder) {
-		return doBuildTree(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
-	}
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return dfs(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1);
+    }
 
-	private TreeNode doBuildTree(int[] inorder, int is, int ie, int[] postorder, int ps, int pe) {
-		if (is > ie)
-			return null;
+    private TreeNode dfs(int[] inorder, int is, int ie, int[] postorder, int ps, int pe) {
+        if (is > ie)
+            return null;
 
-		int rootVal = postorder[pe], leftCount = 0;
-		for (int i = is; i <= ie; i++) {
-			if (inorder[i] == rootVal) {
-				leftCount = i - is;
-				break;
-			}
-		}
+        TreeNode root = new TreeNode(postorder[pe]);
+        int leftLen = 0;
+        for (int i = is; inorder[i] != postorder[pe]; i++)
+            leftLen++;
 
-		TreeNode root = new TreeNode(rootVal);
-		root.left = doBuildTree(inorder, is, is + leftCount - 1, postorder, ps, ps + leftCount - 1);
-		root.right = doBuildTree(inorder, is + leftCount + 1, ie, postorder, ps + leftCount, pe - 1);
-		return root;
-	}
+        root.left = dfs(inorder, is, is + leftLen - 1, postorder, ps, ps + leftLen - 1);
+        root.right = dfs(inorder, is + leftLen + 1, ie, postorder, ps + leftLen, pe - 1);
+        return root;
+    }
 }
