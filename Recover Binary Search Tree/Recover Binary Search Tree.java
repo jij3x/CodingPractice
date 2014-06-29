e32d38bbb93a335598f358deb32f88d74be16216
jij3x <jijex@yahoo.com>
2014-06-29 15:35:21 -0700

    Update Recover Binary Search Tree.java

--- Recover Binary Search Tree/Recover Binary Search Tree.java
+++ Recover Binary Search Tree/Recover Binary Search Tree.java
@@ -8,35 +8,28 @@
  * }
  */
 public class Solution {
-	TreeNode prev, node1, node2;
-	boolean errorDetected;
+    private TreeNode prev, node1, node2;
 
-	public void recoverTree(TreeNode root) {
-		errorDetected = false;
-		inOrderTraversal(root);
+    public void recoverTree(TreeNode root) {
+        prev = node1 = node2 = null;
+        inOrderTraversal(root);
+        int temp = node1.val;
+        node1.val = node2.val;
+        node2.val = temp;
+    }
 
-		int temp = node1.val;
-		node1.val = node2.val;
-		node2.val = temp;
-	}
+    private void inOrderTraversal(TreeNode root) {
+        if (root == null)
+            return;
 
-	private void inOrderTraversal(TreeNode root) {
-		if (root == null)
-			return;
+        inOrderTraversal(root.left);
 
-		inOrderTraversal(root.left);
+        if (prev != null && root.val < prev.val) {
+            node1 = node1 == null ? prev : node1;
+            node2 = root;
+        }
+        prev = root;
 
-		if (prev != null && root.val < prev.val) {
-			if (errorDetected) {
-				node2 = root;
-			} else {
-				node1 = prev;
-				node2 = root;
-				errorDetected = true;
-			}
-		}
-		prev = root;
-
-		inOrderTraversal(root.right);
-	}
+        inOrderTraversal(root.right);
+    }
 }
