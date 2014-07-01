97046cba3f87d9d4f5c397b7f93e57db42b0ea46
jij3x <jijex@yahoo.com>
2014-06-30 22:50:36 -0700

    Update N-Queens II.java

--- N-Queens II/N-Queens II.java
+++ N-Queens II/N-Queens II.java
@@ -1,55 +1,56 @@
 public class Solution {
-	int count, all;
+    private int total;
 
-	public int totalNQueens(int n) {
-		count = 0;
-		all = (1 << n) - 1;
-		doTotalNQueens(0, 0, 0);
-		return count;
-	}
+    public int totalNQueens(int n) {
+        total = 0;
+        int[] board = new int[n];
+        nQueens(board, 0);
+        return total;
+    }
 
-	private void doTotalNQueens(int ld, int row, int rd) {
-		int poss, p;
-		if (row == all) {
-			count++;
-		} else {
-			poss = all & ~(ld | row | rd);
-			while (poss != 0) {
-				p = poss & (-poss); // get poss' right most '1'
-				poss = poss - p;
-				doTotalNQueens((ld | p) << 1, row | p, (rd | p) >> 1);
-			}
-		}
-	}
+    private void nQueens(int[] board, int level) {
+        if (level == board.length) {
+            total++;
+            return;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            boolean qualified = true;
+            for (int j = level - 1; j >= 0; j--) {
+                if (board[j] == i || board[j] == i + (level - j) || board[j] == i - (level - j)) {
+                    qualified = false;
+                    break;
+                }
+            }
+            if (qualified) {
+                board[level] = i;
+                nQueens(board, level + 1);
+            }
+        }
+    }
 }
 
 class Solution2 {
-	public int totalNQueens(int n) {
-		int[] board = new int[n];
-		int[] result = new int[1];
-		doTotalNQueens(n, 0, board, result);
-		return result[0];
-	}
+    int count, all;
 
-	private void doTotalNQueens(int n, int level, int[] board, int[] result) {
-		if (level == n) {
-			result[0]++;
-			return;
-		}
+    public int totalNQueens(int n) {
+        count = 0;
+        all = (1 << n) - 1;
+        doTotalNQueens(0, 0, 0);
+        return count;
+    }
 
-		for (int i = 0; i < n; i++) {
-			boolean validPosition = true;
-			for (int j = level - 1; j >= 0; j--) {
-				if (board[j] == i || board[j] == i + (level - j) || board[j] == i - (level - j)) {
-					validPosition = false;
-					break;
-				}
-			}
-
-			if (validPosition) {
-				board[level] = i;
-				doTotalNQueens(n, level + 1, board, result);
-			}
-		}
-	}
+    private void doTotalNQueens(int ld, int row, int rd) {
+        int poss, p;
+        if (row == all) {
+            count++;
+        } else {
+            poss = all & ~(ld | row | rd);
+            while (poss != 0) {
+                p = poss & (-poss); // get poss' right most '1'
+                poss = poss - p;
+                doTotalNQueens((ld | p) << 1, row | p, (rd | p) >> 1);
+            }
+        }
+    }
 }
