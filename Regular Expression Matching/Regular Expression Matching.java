cee1d2dd5c4ce4c3e8fa97e5822c8859de26bea7
jij3x <jijex@yahoo.com>
2014-07-05 08:23:20 -0700

    Update Regular Expression Matching.java

--- Regular Expression Matching/Regular Expression Matching.java
+++ Regular Expression Matching/Regular Expression Matching.java
@@ -1,32 +1,17 @@
 public class Solution {
-	public boolean isMatch(String s, String p) {
-		return doIsMatch(s, 0, p, 0);
-	}
+    public boolean isMatch(String s, String p) {
+        return match(s, 0, p, 0);
+    }
 
-	private boolean doIsMatch(String s, int si, String p, int pi) {
-		if (si == s.length() && pi == p.length())
-			return true;
-		else if (pi == p.length())
-			return false;
+    private boolean match(String s, int si, String p, int pi) {
+        if (pi == p.length())
+            return si == s.length();
 
-		if (pi == p.length() - 1 || p.charAt(pi + 1) != '*') {
-			if (si >= s.length())
-				return false;
-			return (p.charAt(pi) == '.' || s.charAt(si) == p.charAt(pi)) && doIsMatch(s, si + 1, p, pi + 1);
-		} else {
-			if (doIsMatch(s, si, p, pi + 2))
-				return true;
-			while (si < s.length()) {
-				if (p.charAt(pi) == '.' || s.charAt(si) == p.charAt(pi)) {
-					if (doIsMatch(s, si + 1, p, pi + 2))
-						return true;
-					si++;
-				} else {
-					break;
-				}
-			}
-		}
+        if (pi + 1 < p.length() && p.charAt(pi + 1) == '*') {
+            return (si < s.length() && (s.charAt(si) == p.charAt(pi) || p.charAt(pi) == '.') && match(s, si + 1, p, pi))
+                    || match(s, si, p, pi + 2);
+        }
 
-		return false;
-	}
+        return si < s.length() && (s.charAt(si) == p.charAt(pi) || p.charAt(pi) == '.') && match(s, si + 1, p, pi + 1);
+    }
 }
