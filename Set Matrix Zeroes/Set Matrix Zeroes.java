b22f9828a262869abccbd4d54212030e194a23d4
jij3x <jijex@yahoo.com>
2014-07-05 21:57:56 -0700

    Update Set Matrix Zeroes.java

--- Set Matrix Zeroes/Set Matrix Zeroes.java
+++ Set Matrix Zeroes/Set Matrix Zeroes.java
@@ -1,20 +1,19 @@
 public class Solution {
     public void setZeroes(int[][] matrix) {
-        boolean set1stRow = false, set1stCol = false;
+        boolean clear1stRow = false, clear1stCol = false;
         for (int y = 0; y < matrix.length; y++)
             if (matrix[y][0] == 0)
-                set1stCol = true;
+                clear1stCol = true;
         for (int x = 0; x < matrix[0].length; x++)
             if (matrix[0][x] == 0)
-                set1stRow = true;
+                clear1stRow = true;
 
         for (int y = 1; y < matrix.length; y++) {
-            for (int x = 1; x < matrix[0].length; x++) {
+            for (int x = 1; x < matrix[0].length; x++)
                 if (matrix[y][x] == 0) {
                     matrix[y][0] = 0;
                     matrix[0][x] = 0;
                 }
-            }
         }
 
         for (int y = 1; y < matrix.length; y++) {
@@ -30,11 +29,11 @@ public class Solution {
                     matrix[y][x] = 0;
             }
 
-        if (set1stRow)
+        if (clear1stRow)
             for (int x = 0; x < matrix[0].length; x++)
                 matrix[0][x] = 0;
 
-        if (set1stCol)
+        if (clear1stCol)
             for (int y = 0; y < matrix.length; y++)
                 matrix[y][0] = 0;
     }
