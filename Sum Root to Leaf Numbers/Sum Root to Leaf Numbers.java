ff41cf349d55e794de87a7e433f076102b60bc4f
jij3x <jijex@yahoo.com>
2014-07-06 14:32:16 -0700

    Update Sum Root to Leaf Numbers.java

--- Sum Root to Leaf Numbers/Sum Root to Leaf Numbers.java
+++ Sum Root to Leaf Numbers/Sum Root to Leaf Numbers.java
@@ -8,26 +8,26 @@
  * }
  */
 public class Solution {
-	private int sum;
+    private int sum;
 
-	public int sumNumbers(TreeNode root) {
-		if (root == null)
-			return 0;
+    public int sumNumbers(TreeNode root) {
+        if (root == null)
+            return 0;
 
-		sum = 0;
-		dfs(root, 0);
-		return sum;
-	}
+        sum = 0;
+        dfs(root, 0);
+        return sum;
+    }
 
-	private void dfs(TreeNode root, int past) {
-		if (root.left == null && root.right == null) {
-			sum += past * 10 + root.val;
-			return;
-		}
+    private void dfs(TreeNode root, int num) {
+        if (root.left == null && root.right == null) {
+            sum += num * 10 + root.val;
+            return;
+        }
 
-		if (root.left != null)
-			dfs(root.left, past * 10 + root.val);
-		if (root.right != null)
-			dfs(root.right, past * 10 + root.val);
-	}
+        if (root.left != null)
+            dfs(root.left, num * 10 + root.val);
+        if (root.right != null)
+            dfs(root.right, num * 10 + root.val);
+    }
 }
