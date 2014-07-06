5d1d27a1c17d72a713b7870f5350ee1d43150f58
jij3x <jijex@yahoo.com>
2014-07-05 21:23:11 -0700

    Update Search a 2D Matrix.java

--- Search a 2D Matrix/Search a 2D Matrix.java
+++ Search a 2D Matrix/Search a 2D Matrix.java
@@ -1,43 +1,43 @@
 public class Solution {
-	public boolean searchMatrix(int[][] matrix, int target) {
-		int m = matrix.length, n = matrix[0].length, start = 0, end = m - 1, mid = 0;
-		while (start <= end) {
-			mid = (start + end) / 2;
-			if (matrix[mid][0] > target) {
-				end = mid - 1;
-			} else if (matrix[mid][n - 1] < target) {
-				start = mid + 1;
-			} else {
-				start = 0;
-				end = n - 1;
-				while (start <= end) {
-					int c = (start + end) / 2;
-					if (matrix[mid][c] > target)
-						end = c - 1;
-					else if (matrix[mid][c] < target)
-						start = c + 1;
-					else
-						return true;
-				}
-				return false;
-			}
-		}
-		return false;
-	}
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length, start = 0, end = m - 1, mid = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (matrix[mid][0] > target) {
+                end = mid - 1;
+            } else if (matrix[mid][n - 1] < target) {
+                start = mid + 1;
+            } else {
+                start = 0;
+                end = n - 1;
+                while (start <= end) {
+                    int c = (start + end) / 2;
+                    if (matrix[mid][c] > target)
+                        end = c - 1;
+                    else if (matrix[mid][c] < target)
+                        start = c + 1;
+                    else
+                        return true;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
 }
 
 class Solution2 {
-	public boolean searchMatrix(int[][] matrix, int target) {
-		for (int rowCnt = matrix[0].length, start = 0, end = matrix.length * rowCnt - 1; start <= end;) {
-			int mid = (start + end) / 2;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int rowCnt = matrix[0].length, start = 0, end = matrix.length * rowCnt - 1; start <= end;) {
+            int mid = (start + end) / 2;
 
-			if (matrix[mid / rowCnt][mid % rowCnt] > target)
-				end = mid - 1;
-			else if (matrix[mid / rowCnt][mid % rowCnt] < target)
-				start = mid + 1;
-			else
-				return true;
-		}
-		return false;
-	}
+            if (matrix[mid / rowCnt][mid % rowCnt] > target)
+                end = mid - 1;
+            else if (matrix[mid / rowCnt][mid % rowCnt] < target)
+                start = mid + 1;
+            else
+                return true;
+        }
+        return false;
+    }
 }
