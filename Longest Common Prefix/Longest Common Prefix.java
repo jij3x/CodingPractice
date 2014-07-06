e091414cffc1d85c53246a0a67c758736af221ca
jij3x <jijex@yahoo.com>
2014-07-06 14:38:58 -0700

    Update Longest Common Prefix.java

--- Longest Common Prefix/Longest Common Prefix.java
+++ Longest Common Prefix/Longest Common Prefix.java
@@ -1,16 +1,16 @@
 public class Solution {
-	public String longestCommonPrefix(String[] strs) {
-		if (strs.length == 0)
-			return "";
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0)
+            return "";
 
-		int end = 0;
-		done: while (true) {
-			for (int i = 0; i < strs.length; i++) {
-				if (strs[i].length() == end || (i != 0 && strs[i].charAt(end) != strs[i - 1].charAt(end)))
-					break done;
-			}
-			end++;
-		}
-		return strs[0].substring(0, end);
-	}
+        StringBuilder prefix = new StringBuilder();
+        for (int i = 0; i < strs[0].length(); i++) {
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != strs[0].charAt(i))
+                    return prefix.toString();
+            }
+            prefix.append(strs[0].charAt(i));
+        }
+        return prefix.toString();
+    }
 }
