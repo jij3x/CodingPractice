df256864480c5cc4f1872c7da107b3bac009cd84
jij3x <jijex@yahoo.com>
2014-07-07 16:26:05 -0700

    Update Partition List.java

--- Partition List/Partition List.java
+++ Partition List/Partition List.java
@@ -10,47 +10,20 @@
  * }
  */
 public class Solution {
-	public ListNode partition(ListNode head, int x) {
-		ListNode greater = new ListNode(0), greaterTail = greater;
-		ListNode less = new ListNode(0), lessTail = less;
-
-		less.next = head;
-		while (head != null) {
-			if (head.val >= x) {
-				lessTail.next = head.next;
-				greaterTail.next = head;
-				greaterTail = head;
-				greaterTail.next = null;
-				head = lessTail.next;
-			} else {
-				lessTail = head;
-				head = head.next;
-			}
-		}
-		lessTail.next = greater.next;
-		return less.next;
-	}
-}
-
-class Solution2 {
-	public ListNode partition(ListNode head, int x) {
-		ListNode greater = new ListNode(0), greaterTail = greater;
-		ListNode less = new ListNode(0), lessTail = less;
-
-		while (head != null) {
-			if (head.val < x) {
-				lessTail.next = head;
-				lessTail = head;
-			} else {
-				greaterTail.next = head;
-				greaterTail = head;
-			}
-			head = head.next;
-			lessTail.next = null;
-			greaterTail.next = null;
-		}
-
-		lessTail.next = greater.next;
-		return less.next;
-	}
+    public ListNode partition(ListNode head, int x) {
+        ListNode less = new ListNode(0), greater = new ListNode(0), lp = less, gp = greater;
+        while (head != null) {
+            if (head.val < x) {
+                lp.next = head;
+                lp = lp.next;
+            } else {
+                gp.next = head;
+                gp = gp.next;
+            }
+            head = head.next;
+        }
+        gp.next = null;
+        lp.next = greater.next;
+        return less.next;
+    }
 }
