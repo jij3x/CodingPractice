d9dc6edc2fca1e454657c683ad166ffdee6a1ba5
jij3x <jijex@yahoo.com>
2014-07-07 22:35:43 -0700

    Update Text Justification.java

--- Text Justification/Text Justification.java
+++ Text Justification/Text Justification.java
@@ -1,47 +1,81 @@
 public class Solution {
-	public ArrayList<String> fullJustify(String[] words, int L) {
-		ArrayList<String> result = new ArrayList<String>();
-		int start = 0;
-		for (int i = 1, spaceLeft = L - words[0].length(); i < words.length; i++) {
-			if (spaceLeft - words[i].length() - 1 < 0) {
-				result.add(lineJustify(words, L, start, i - 1));
-				spaceLeft = L - words[i].length();
-				start = i;
-			} else {
-				spaceLeft -= words[i].length() + 1;
-			}
-		}
-		result.add(lastLineJustify(words, L, start));
-		return result;
-	}
+    public List<String> fullJustify(String[] words, int L) {
+        ArrayList<String> result = new ArrayList<String>();
+        int start = 0;
+        for (int i = 1, spaceLeft = L - words[0].length(); i < words.length; i++) {
+            if (spaceLeft - words[i].length() - 1 < 0) {
+                result.add(lineJustify(words, L, start, i - 1));
+                spaceLeft = L - words[i].length();
+                start = i;
+            } else {
+                spaceLeft -= words[i].length() + 1;
+            }
+        }
+        result.add(lastLineJustify(words, L, start));
+        return result;
+    }
 
-	private String lineJustify(String[] words, int L, int start, int end) {
-		int totalPads = L;
-		for (int i = start; i <= end; i++)
-			totalPads -= words[i].length();
+    private String lineJustify(String[] words, int L, int start, int end) {
+        int totalPads = L;
+        for (int i = start; i <= end; i++)
+            totalPads -= words[i].length();
 
-		String line = words[start++];
-		for (int i = start; i <= end; i++) {
-			int padCount = totalPads / (end + 1 - i) + (totalPads % (end + 1 - i) == 0 ? 0 : 1);
-			totalPads -= padCount;
-			for (int j = 0; j < padCount; j++)
-				line += ' ';
-			line += words[i];
-		}
+        StringBuilder line = new StringBuilder(words[start++]);
+        for (int i = start; i <= end; i++) {
+            int padCount = totalPads / (end + 1 - i) + (totalPads % (end + 1 - i) == 0 ? 0 : 1);
+            totalPads -= padCount;
+            for (int j = 0; j < padCount; j++)
+                line.append(" ");
+            line.append(words[i]);
+        }
 
-		// pad whitespace, in case of only one word.
-		for (int i = line.length(); i < L; i++)
-			line += " ";
-		return line;
-	}
+        // pad whitespace, in case of only one word.
+        for (int i = line.length(); i < L; i++)
+            line.append(" ");
+        return line.toString();
+    }
 
-	private String lastLineJustify(String[] words, int L, int start) {
-		String line = "";
-		for (int i = start; i < words.length; i++)
-			line += line.isEmpty() ? words[i] : " " + words[i];
+    private String lastLineJustify(String[] words, int L, int start) {
+        StringBuilder line = new StringBuilder();
+        for (int i = start; i < words.length; i++)
+            line.append(line.length() == 0 ? "" : " ").append(words[i]);
 
-		for (int i = line.length(); i < L; i++)
-			line += " ";
-		return line;
-	}
+        for (int i = line.length(); i < L; i++)
+            line.append(" ");
+        return line.toString();
+    }
+}
+
+class Solution2 {
+    public List<String> fullJustify(String[] words, int L) {
+        ArrayList<String> result = new ArrayList<String>();
+        int left = 0;
+        for (int i = 1, len = words[0].length(); i < words.length; i++) {
+            if (len + words[i].length() + (i - left) > L) {
+                StringBuilder line = new StringBuilder();
+                for (int j = left, spCnt = L - len; j < i; j++) {
+                    line.append(words[j]);
+
+                    int sp = j == i - 1 ? spCnt : spCnt / (i - j - 1) + ((spCnt % (i - j - 1)) > 0 ? 1 : 0);
+                    for (int k = sp; k > 0; k--)
+                        line.append(' ');
+                    spCnt -= sp;
+                }
+                result.add(line.toString());
+
+                left = i;
+                len = words[i].length();
+            } else {
+                len += words[i].length();
+            }
+        }
+
+        StringBuilder line = new StringBuilder(words[left++]);
+        while (left < words.length)
+            line.append(' ').append(words[left++]);
+        for (int i = line.length(); i < L; i++)
+            line.append(' ');
+        result.add(line.toString());
+        return result;
+    }
 }
