a75b555ddf32dbcb2e71db1ed747fa9b0c3e71b8
jij3x <jijex@yahoo.com>
2014-07-10 08:11:47 -0700

    Update Binary Tree Inorder Traversal.java

--- Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.java
+++ Binary Tree Inorder Traversal/Binary Tree Inorder Traversal.java
@@ -8,38 +8,38 @@
  * }
  */
 public class Solution {
-	public ArrayList<Integer> inorderTraversal(TreeNode root) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		Stack<TreeNode> parentStack = new Stack<TreeNode>();
-		TreeNode curr = root;
-		while (!parentStack.isEmpty() || curr != null) {
-			if (curr != null) {
-				parentStack.push(curr);
-				curr = curr.left;
-			} else {
-				curr = parentStack.pop();
-				result.add(curr.val);
-				curr = curr.right;
-			}
-		}
-		return result;
-	}
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        TreeNode node = root;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        while (!stack.isEmpty() || node != null) {
+            if (node != null) {
+                stack.push(node);
+                node = node.left;
+            } else {
+                node = stack.pop();
+                result.add(node.val);
+                node = node.right;
+            }
+        }
+        return result;
+    }
 }
 
 class Solution2 {
-	public ArrayList<Integer> inorderTraversal(TreeNode root) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		doInorderTraversal(root, result);
+    public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        doInorderTraversal(root, result);
 
-		return result;
-	}
+        return result;
+    }
 
-	private void doInorderTraversal(TreeNode root, ArrayList<Integer> result) {
-		if (root == null)
-			return;
+    private void doInorderTraversal(TreeNode root, ArrayList<Integer> result) {
+        if (root == null)
+            return;
 
-		doInorderTraversal(root.left, result);
-		result.add(root.val);
-		doInorderTraversal(root.right, result);
-	}
+        doInorderTraversal(root.left, result);
+        result.add(root.val);
+        doInorderTraversal(root.right, result);
+    }
 }
