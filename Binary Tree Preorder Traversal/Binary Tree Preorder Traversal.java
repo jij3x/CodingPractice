6c54fe9a9cb048f24dab48081aeb829077767fe7
jij3x <jijex@yahoo.com>
2014-07-10 07:46:05 -0700

    Update Binary Tree Preorder Traversal.java

--- Binary Tree Preorder Traversal/Binary Tree Preorder Traversal.java
+++ Binary Tree Preorder Traversal/Binary Tree Preorder Traversal.java
@@ -8,35 +8,35 @@
  * }
  */
 public class Solution {
-	public ArrayList<Integer> preorderTraversal(TreeNode root) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		Stack<TreeNode> parentStack = new Stack<TreeNode>();
-		parentStack.push(root);
-		while (!parentStack.isEmpty()) {
-			TreeNode curr = parentStack.pop();
-			if (curr != null) {
-				result.add(curr.val);
-				parentStack.push(curr.right);
-				parentStack.push(curr.left);
-			}
-		}
-		return result;
-	}
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode curr = stack.pop();
+            if (curr != null) {
+                result.add(curr.val);
+                stack.push(curr.right);
+                stack.push(curr.left);
+            }
+        }
+        return result;
+    }
 }
 
 class Solution2 {
-	public ArrayList<Integer> preorderTraversal(TreeNode root) {
-		ArrayList<Integer> result = new ArrayList<Integer>();
-		doPreorderTraversal(root, result);
-		return result;
-	}
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        doPreorderTraversal(root, result);
+        return result;
+    }
 
-	private void doPreorderTraversal(TreeNode root, ArrayList<Integer> result) {
-		if (root == null)
-			return;
+    private void doPreorderTraversal(TreeNode root, ArrayList<Integer> result) {
+        if (root == null)
+            return;
 
-		result.add(root.val);
-		doPreorderTraversal(root.left, result);
-		doPreorderTraversal(root.right, result);
-	}
+        result.add(root.val);
+        doPreorderTraversal(root.left, result);
+        doPreorderTraversal(root.right, result);
+    }
 }
