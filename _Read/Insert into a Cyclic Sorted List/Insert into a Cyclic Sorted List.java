90973877b19a4aca5fe7cc9fb28cf9e5bd9d256f
jij3x <jijex@yahoo.com>
2014-07-12 22:36:06 -0700

    upload

---
+++ _Read/Insert into a Cyclic Sorted List/Insert into a Cyclic Sorted List.java
@@ -0,0 +1,23 @@
+public class Solution {
+    void insert(ListNode node, int x) {
+        if (node == null) {
+            node = new ListNode(x);
+            node.next = node;
+            return;
+        }
+
+        ListNode prev = node, curr = node.next;
+        while (curr != node) {
+            if (x >= prev.val && x <= curr.val)
+                break;
+            if (prev.val > curr.val && (x < curr.val || x > prev.val))
+                break;
+            prev = prev.next;
+            curr = curr.next;
+        }
+
+        ListNode newNode = new ListNode(x);
+        newNode.next = curr;
+        prev.next = newNode;
+    }
+}
