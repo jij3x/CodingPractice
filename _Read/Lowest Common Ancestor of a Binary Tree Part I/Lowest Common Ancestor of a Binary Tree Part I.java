388397b40ed7316022bbff23bd32c61382b8945a
jij3x <jijex@yahoo.com>
2014-07-18 14:51:20 -0700

    Update Lowest Common Ancestor of a Binary Tree Part I.java

--- _Read/Lowest Common Ancestor of a Binary Tree Part I/Lowest Common Ancestor of a Binary Tree Part I.java
+++ _Read/Lowest Common Ancestor of a Binary Tree Part I/Lowest Common Ancestor of a Binary Tree Part I.java
@@ -11,4 +11,28 @@ public class Solution {
             return root;
         return left != null ? left : right;
     }
+
+    public TreeNodeWithParent LCA(TreeNodeWithParent p, TreeNodeWithParent q) {
+        int pHeight = 0, qHeight = 0;
+        for (TreeNodeWithParent curr = p; curr != null; curr = curr.parent)
+            pHeight++;
+        for (TreeNodeWithParent curr = q; curr != null; curr = curr.parent)
+            qHeight++;
+
+        if (pHeight > qHeight) {
+            for (int i = 0; i < pHeight - qHeight; i++)
+                p = p.parent;
+        } else {
+            for (int i = 0; i < qHeight - pHeight; i++)
+                q = q.parent;
+        }
+
+        while (p != null) {
+            if (p == q)
+                return p;
+            p = p.parent;
+            q = q.parent;
+        }
+        return null;
+    }
 }
