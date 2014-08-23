73154179b0699912d99bf4a507a63693c8b92f6b
jij3x <jijex@yahoo.com>
2014-08-22 17:34:57 -0700

    Update CTRL+A CTRL+C CTRL+V.java

--- _Read/CTRL+A CTRL+C CTRL+V/CTRL+A CTRL+C CTRL+V.java
+++ _Read/CTRL+A CTRL+C CTRL+V/CTRL+A CTRL+C CTRL+V.java
@@ -47,4 +47,72 @@ public class Solution {
         }
         return combo;
     }
+
+    public List<String> printCombo2(int n) {
+        String[] memo = new String[n + 1];
+        memo[1] = "1A";
+        memo[2] = "2A";
+        memo[3] = "3A";
+
+        dpPrintCombo(n, memo);
+
+        return convertCombo(memo[n]);
+    }
+
+    private void dpPrintCombo(int n, String[] memo) {
+        if (n <= 3)
+            return;
+
+        for (int i = 4; i <= n; i++) {
+            memo[i] = Integer.toString(i) + "A";
+
+            for (int j = i - 4; j > 1; j--) {
+                String subCombo = memo[j];
+                if (comboLength(subCombo) * (i - j - 2) > comboLength(memo[i])) {
+                    memo[i] = subCombo + Integer.toString(i - j - 2) + "D";
+                }
+            }
+        }
+    }
+
+    private long comboLength(String combo) {
+        long result = 0;
+        boolean aNotion = true;
+        for (int i = 0, d = 0; i < combo.length(); i++) {
+            if (combo.charAt(i) == 'A') {
+                aNotion = false;
+            } else if (combo.charAt(i) == 'D') {
+                result *= d;
+                d = 0;
+            } else if (aNotion) {
+                result = result * 10 + (combo.charAt(i) - '0');
+            } else {
+                d = d * 10 + (combo.charAt(i) - '0');
+            }
+        }
+        return result;
+    }
+
+    private List<String> convertCombo(String combo) {
+        List<String> result = new ArrayList<String>();
+        boolean aNotion = true;
+        for (int i = 0, a = 0, d = 0; i < combo.length(); i++) {
+            if (combo.charAt(i) == 'A') {
+                for (int j = a; j > 0; j--)
+                    result.add("A");
+                aNotion = false;
+            } else if (combo.charAt(i) == 'D') {
+                result.add("CTRL+A");
+                result.add("CTRL+C");
+                for (int j = d; j > 0; j--)
+                    result.add("CTRL+V");
+                d = 0;
+            } else if (aNotion) {
+                a = a * 10 + (combo.charAt(i) - '0');
+            } else {
+                d = d * 10 + (combo.charAt(i) - '0');
+            }
+        }
+        return result;
+    }
 }
