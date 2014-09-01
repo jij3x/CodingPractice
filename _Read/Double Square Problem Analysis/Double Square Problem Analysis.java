ab8245cc555f081f39d8018bb74bb1487a14704c
jij3x <jijex@yahoo.com>
2014-09-01 08:16:48 -0700

    Update Double Square Problem Analysis.java

--- _Read/Double Square Problem Analysis/Double Square Problem Analysis.java
+++ _Read/Double Square Problem Analysis/Double Square Problem Analysis.java
@@ -1,5 +1,16 @@
 public class Solution {
     public int doubleSquare(int m) {
+        int limit = (int) Math.sqrt(m / 2.0);
+        int total = 0;
+        for (int i = 0; i <= limit; i++) {
+            int j = (int) Math.sqrt(m - i * i);
+            if (j * j + i * i == m)
+                total++;
+        }
+        return total;
+    }
+
+    public int doubleSquare2(int m) {
         int total = 0;
         for (int i = 0; i <= (int) Math.sqrt(m); i++) {
             for (int j = 0; j <= i; j++) {
