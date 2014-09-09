dc7a2403cba88a24c16d10aeffbe2be9737f5777
jij3x <jijex@yahoo.com>
2014-09-09 00:15:01 -0700

    Update Search in Rotated Sorted Array.java

--- Search in Rotated Sorted Array/Search in Rotated Sorted Array.java
+++ Search in Rotated Sorted Array/Search in Rotated Sorted Array.java
@@ -1,18 +1,17 @@
 public class Solution {
     public int search(int[] A, int target) {
-        int start = 0, end = A.length - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
+        for (int start = 0, end = A.length - 1, mid = 0; start <= end;) {
+            mid = start + (end - start) / 2;
             if (A[mid] == target)
                 return mid;
 
             if (A[mid] < A[end]) {
-                if (A[mid] < target && A[end] >= target)
+                if (target > A[mid] && target <= A[end])
                     start = mid + 1;
                 else
                     end = mid - 1;
             } else {
-                if (A[start] <= target && A[mid] > target)
+                if (target >= A[start] && target < A[mid])
                     end = mid - 1;
                 else
                     start = mid + 1;
