664bd8a1112464646677c9d6873e0f721a7a829b
jij3x <jijex@yahoo.com>
2014-09-08 21:25:07 -0700

    Update Permutations.java

--- Permutations/Permutations.java
+++ Permutations/Permutations.java
@@ -28,26 +28,24 @@ public class Solution {
 }
 
 class Solution2 {
-    public ArrayList<ArrayList<Integer>> permute(int[] num) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> path = new ArrayList<Integer>();
+    public List<List<Integer>> permute(int[] num) {
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
         boolean[] visited = new boolean[num.length];
-
-        doPermute(num, visited, path, result);
+        doPermute(num, visited, new ArrayList<Integer>(), result);
         return result;
     }
 
-    private void doPermute(int[] num, boolean[] visited, ArrayList<Integer> path, ArrayList<ArrayList<Integer>> result) {
+    private void doPermute(int[] num, boolean[] visited, List<Integer> path, List<List<Integer>> result) {
         if (path.size() == num.length)
             result.add(new ArrayList<Integer>(path));
 
         for (int i = 0; i < num.length; i++) {
             if (!visited[i]) {
-                path.add(num[i]);
                 visited[i] = true;
+                path.add(num[i]);
                 doPermute(num, visited, path, result);
-                path.remove(path.size() - 1);
                 visited[i] = false;
+                path.remove(path.size() - 1);
             }
         }
     }
