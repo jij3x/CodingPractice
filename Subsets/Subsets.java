f1caca76399c95b6898754cf23f29ae5b274dbf2
jij3x <jijex@yahoo.com>
2014-09-09 22:40:50 -0700

    Update Subsets.java

--- Subsets/Subsets.java
+++ Subsets/Subsets.java
@@ -4,12 +4,12 @@ public class Solution {
         result.add(new ArrayList<Integer>());
 
         Arrays.sort(S);
-        for (int i = 0; i < S.length; i++) {
+        for (int e : S) {
             int len = result.size();
-            for (int j = 0; j < len; j++) {
-                List<Integer> r = new ArrayList<Integer>(result.get(j));
-                r.add(S[i]);
-                result.add(r);
+            for (int i = 0; i < len; i++) {
+                ArrayList<Integer> row = new ArrayList<Integer>(result.get(i));
+                row.add(e);
+                result.add(row);
             }
         }
         return result;
@@ -17,6 +17,29 @@ public class Solution {
 }
 
 class Solution2 {
+    public List<List<Integer>> subsets(int[] S) {
+        Arrays.sort(S);
+        return doSubsets(S, 0);
+    }
+
+    private List<List<Integer>> doSubsets(int[] S, int start) {
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (start == S.length) {
+            result.add(new ArrayList<Integer>());
+            return result;
+        }
+
+        for (List<Integer> row : doSubsets(S, start + 1)) {
+            result.add(row);
+            ArrayList<Integer> dup = new ArrayList<Integer>(row);
+            dup.add(0, S[start]);
+            result.add(dup);
+        }
+        return result;
+    }
+}
+
+class Solution3 {
     public List<List<Integer>> subsets(int[] S) {
         Arrays.sort(S);
         return doSubsets(S, 0);
