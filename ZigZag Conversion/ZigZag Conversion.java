838ea47b17abd3e06cb117766658726693619376
jij3x <jijex@yahoo.com>
2014-10-02 07:45:22 -0700

    Update ZigZag Conversion.java

--- ZigZag Conversion/ZigZag Conversion.java
+++ ZigZag Conversion/ZigZag Conversion.java
@@ -56,13 +56,10 @@ class Solution3 {
 
         char[] result = new char[s.length()];
         for (int i = 0, p = 0; i < nRows; i++) {
-            for (int j = 0, index = i; index < s.length(); j++, index = (2 * nRows - 2) * j + i) {
-                result[p++] = s.charAt(index);
-                if (i == 0 || i == nRows - 1)
-                    continue;
-
-                if (index + (nRows - i - 1) * 2 < s.length())
-                    result[p++] = s.charAt(index + (nRows - i - 1) * 2);
+            for (int j = 0, idx = i; idx < s.length(); j++, idx = (2 * nRows - 2) * j + i) {
+                result[p++] = s.charAt(idx);
+                if (i != 0 && i != nRows - 1 && idx + (nRows - i - 1) * 2 < s.length())
+                    result[p++] = s.charAt(idx + (nRows - i - 1) * 2);
             }
         }
         return new String(result);
