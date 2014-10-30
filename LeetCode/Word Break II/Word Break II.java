81018519fb35330d85b5974ef360d9458fe24c3d
jij3x <jijex@yahoo.com>
2014-10-30 07:49:27 -0700

    Update Word Break II.java

--- LeetCode/Word Break II/Word Break II.java
+++ LeetCode/Word Break II/Word Break II.java
@@ -1,61 +1,19 @@
 public class Solution {
-    public ArrayList<String> wordBreak(String s, Set<String> dict) {
-        byte[] memo = new byte[s.length() + 1];
-        memo[s.length()] = 1;
+    public List<String> wordBreak(String s, Set<String> dict) {
+        ArrayList<ArrayList<String>> memo = new ArrayList<ArrayList<String>>();
+        for (int i = 0; i <= s.length(); i++)
+            memo.add(new ArrayList<String>());
+        memo.get(s.length()).add("");
 
-        return doWordBreak(s, 0, dict, memo);
-    }
-
-    public ArrayList<String> doWordBreak(String s, int start, Set<String> dict, byte[] memo) {
-        ArrayList<String> result = new ArrayList<String>();
-        if (start == s.length()) {
-            result.add("");
-            return result;
-        }
-
-        for (int i = start + 1; i <= s.length(); i++) {
-            String word = s.substring(start, i);
-            if ((memo[i]) != -1 && dict.contains(word)) {
-                for (String wordList : doWordBreak(s, i, dict, memo)) {
-                    result.add(word + (wordList.isEmpty() ? "" : " " + wordList));
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                String word = s.substring(i, j + 1);
+                if (dict.contains(word) && memo.get(j + 1).size() > 0) {
+                    for (String wdList : memo.get(j + 1))
+                        memo.get(i).add(word + (wdList.isEmpty() ? "" : " ") + wdList);
                 }
             }
         }
-
-        memo[start] = (byte) (result.size() == 0 ? -1 : 1);
-        return result;
-    }
-}
-
-class Solution2 {
-    public ArrayList<String> wordBreak(String s, Set<String> dict) {
-        boolean[] memo = new boolean[s.length() + 1];
-        memo[s.length()] = true;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                if (memo[j] && dict.contains(s.substring(i, j)))
-                    memo[i] = true;
-            }
-        }
-
-        ArrayList<String> result = new ArrayList<String>();
-        StringBuilder path = new StringBuilder();
-        dpWordBreak(s, dict, 0, path, result, memo);
-        return result;
-    }
-
-    private void dpWordBreak(String s, Set<String> dict, int start, StringBuilder path, ArrayList<String> result,
-            boolean[] memo) {
-        if (start == s.length())
-            result.add(path.toString());
-
-        for (int i = start + 1; i <= s.length(); i++) {
-            if (memo[i] && dict.contains(s.substring(start, i))) {
-                int len = path.length();
-                path.append(path.length() == 0 ? s.substring(start, i) : " " + s.substring(start, i));
-                dpWordBreak(s, dict, i, path, result, memo);
-                path.delete(len, path.length());
-            }
-        }
+        return memo.get(0);
     }
 }
