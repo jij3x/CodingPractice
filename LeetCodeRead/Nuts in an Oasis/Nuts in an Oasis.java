20046f0a43d080d5821a7de6d26571512231d013
jij3x <jijex@yahoo.com>
2014-11-10 22:44:34 -0800

    Update Nuts in an Oasis.java

--- LeetCodeRead/Nuts in an Oasis/Nuts in an Oasis.java
+++ LeetCodeRead/Nuts in an Oasis/Nuts in an Oasis.java
@@ -3,25 +3,13 @@ public class Solution {
         if (N <= C)
             return Math.max(0, N - D * F);
 
-        if (N % C == 0) {
-            int numTrips = (int) (N / C - 1) * 2 + 1;
-            double traveled = C / numTrips / F;
-            if (traveled >= D)
-                return N - numTrips * D * F;
+        int numTrips = (int) (2 * (Math.ceil(N / C) - 1) + 1);
+        double costPerKm = numTrips * F;
+        double remainingNuts = C * (Math.ceil(N / C) - 1);
+        double traveled = (N - remainingNuts) / costPerKm;
 
-            getMaxNuts(N - C, D - traveled, C, F);
-        }
-
-        int numTrips = 2 * ((int) Math.ceil(N / C) - 1) + 1;
-        double traveled = C / numTrips / F;
         if (traveled >= D)
-            return Math.max(N - numTrips * D * F, Math.floor(N / C) * C - (numTrips - 2) * D * F);
-
-        double remainingNuts = N - C;
-        if (N % C != 0 && N - C < Math.floor(N / C) * C - C * (numTrips - 2) / numTrips) {
-            remainingNuts = Math.floor(N / C) * C - C * (numTrips - 2) / numTrips;
-            numTrips -= 2;
-        }
+            return N - D * costPerKm;
 
         return getMaxNuts(remainingNuts, D - traveled, C, F);
     }
