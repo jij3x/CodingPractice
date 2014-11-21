7307cf37488c30e1cb6fc43abfd62f18ddf9fa9e
jij3x <jijex@yahoo.com>
2014-11-21 07:59:17 -0800

    Update 4Sum.java

--- LeetCode/4Sum/4Sum.java
+++ LeetCode/4Sum/4Sum.java
@@ -1,27 +1,26 @@
 public class Solution {
-    public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
+    public List<List<Integer>> fourSum(int[] num, int target) {
         Arrays.sort(num);
 
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        for (int i = 0; i <= num.length - 4; i++) {
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i < num.length; i++) {
             if (i > 0 && num[i] == num[i - 1])
                 continue;
 
-            for (int j = i + 1; j <= num.length - 3; j++) {
+            for (int j = i + 1; j < num.length; j++) {
                 if (j > i + 1 && num[j] == num[j - 1])
                     continue;
 
-                int start = j + 1, end = num.length - 1;
-                while (start < end) {
-                    if (num[i] + num[j] + num[start] + num[end] == target) {
-                        Integer[] r = { num[i], num[j], num[start++], num[end--] };
-                        result.add(new ArrayList<Integer>(Arrays.asList(r)));
+                for (int start = j + 1, end = num.length - 1, tgt = target - num[i] - num[j]; start < end;) {
+                    if (num[start] + num[end] == tgt) {
+                        Integer[] row = { num[i], num[j], num[start++], num[end--] };
+                        result.add(new ArrayList<Integer>(Arrays.asList(row)));
 
                         while (start < end && num[start] == num[start - 1])
                             start++;
                         while (start < end && num[end] == num[end + 1])
                             end--;
-                    } else if (num[i] + num[j] + num[start] + num[end] > target) {
+                    } else if (num[start] + num[end] > tgt) {
                         end--;
                     } else {
                         start++;
