e963f5dc4a2ff1c03c0bf20ce876b42198a39682
jij3x <jijex@yahoo.com>
2014-11-21 07:30:56 -0800

    Update Two Sum.java

--- LeetCode/Two Sum/Two Sum.java
+++ LeetCode/Two Sum/Two Sum.java
@@ -1,14 +1,14 @@
 public class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int[] result = new int[2];
-        HashMap<Integer, Integer> tbl = new HashMap<Integer, Integer>();
+        HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
         for (int i = 0; i < numbers.length; i++) {
-            if (tbl.containsKey(target - numbers[i])) {
-                result[0] = tbl.get(target - numbers[i]) + 1;
+            if (memo.containsKey(target - numbers[i])) {
+                result[0] = memo.get(target - numbers[i]) + 1;
                 result[1] = i + 1;
                 break;
             }
-            tbl.put(numbers[i], i);
+            memo.put(numbers[i], i);
         }
         return result;
     }
