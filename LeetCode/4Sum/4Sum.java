9f7fd31a0d32431d6f94fee28cb01a035a56274a
jij3x <jijex@yahoo.com>
2014-11-21 21:37:31 -0800

    Update 4Sum.java

--- LeetCode/4Sum/4Sum.java
+++ LeetCode/4Sum/4Sum.java
@@ -40,48 +40,27 @@ class Solution2 {
             this.a = a;
             this.b = b;
         }
-
-        @Override
-        public boolean equals(Object other) {
-            if (!(other instanceof Pair))
-                return false;
-            return this.a == ((Pair) other).a && this.b == ((Pair) other).b;
-        }
-
-        @Override
-        public int hashCode() {
-            return (23 * 31 + this.a) * 31 + this.b;
-        }
     }
 
-    public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
-        HashMap<Integer, HashSet<Pair>> lookupTbl = new HashMap<Integer, HashSet<Pair>>();
+    public List<List<Integer>> fourSum(int[] num, int target) {
+        HashMap<Integer, ArrayList<Pair>> lookupTbl = new HashMap<Integer, ArrayList<Pair>>();
         for (int i = 0; i < num.length - 1; i++) {
             for (int j = i + 1; j < num.length; j++) {
                 int key = num[i] + num[j];
-                HashSet<Pair> pairSet = null;
-                if (lookupTbl.containsKey(key)) {
-                    pairSet = lookupTbl.get(key);
-                } else {
-                    pairSet = new HashSet<Pair>();
-                    lookupTbl.put(key, pairSet);
-                }
-                pairSet.add(new Pair(i, j));
+                if (!lookupTbl.containsKey(key))
+                    lookupTbl.put(key, new ArrayList<Pair>());
+                lookupTbl.get(key).add(new Pair(i, j));
             }
         }
 
         HashSet<ArrayList<Integer>> resultSet = new HashSet<ArrayList<Integer>>();
         for (Integer key : lookupTbl.keySet()) {
             int tgtKey = target - key;
-            if (!lookupTbl.containsKey(tgtKey) || lookupTbl.get(tgtKey).size() == 0)
+            if (!lookupTbl.containsKey(tgtKey))
                 continue;
 
-            HashSet<Pair> pairSet1 = lookupTbl.get(key);
-            HashSet<Pair> pairSet2 = lookupTbl.get(tgtKey);
-            for (Iterator<Pair> i1 = pairSet1.iterator(); i1.hasNext();) {
-                Pair pair1 = i1.next();
-                for (Iterator<Pair> i2 = pairSet2.iterator(); i2.hasNext();) {
-                    Pair pair2 = i2.next();
+            for (Pair pair1 : lookupTbl.get(key)) {
+                for (Pair pair2 : lookupTbl.get(tgtKey)) {
                     if (pair1.a == pair2.a || pair1.a == pair2.b || pair1.b == pair2.a || pair1.b == pair2.b)
                         continue;
                     Integer[] r = { num[pair1.a], num[pair1.b], num[pair2.a], num[pair2.b] };
@@ -89,15 +68,7 @@ class Solution2 {
                     resultSet.add(new ArrayList<Integer>(Arrays.asList(r)));
                 }
             }
-
-            pairSet1.clear();
-            pairSet2.clear();
         }
-
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        for (Iterator<ArrayList<Integer>> it = resultSet.iterator(); it.hasNext();) {
-            result.add(it.next());
-        }
-        return result;
+        return new ArrayList<List<Integer>>(resultSet);
     }
 }
