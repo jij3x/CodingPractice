f56b036571cde12d5f296a7be233ca673d909dc3
jij3x <jijex@yahoo.com>
2014-11-22 23:01:38 -0800

    Update Permutation Sequence.java

--- LeetCode/Permutation Sequence/Permutation Sequence.java
+++ LeetCode/Permutation Sequence/Permutation Sequence.java
@@ -4,27 +4,6 @@ public class Solution {
         for (int i = 0; i <= n; i++)
             fTbl[i] = i < 2 ? 1 : fTbl[i - 1] * i;
 
-        boolean[] visited = new boolean[n];
-        int result = 0;
-        for (; n > 0; k = (k - 1) % fTbl[n - 1] + 1, n--) {
-            for (int i = 0, seg = (k - 1) / fTbl[n - 1]; i < visited.length; i++) {
-                if (!visited[i] && --seg < 0) {
-                    visited[i] = true;
-                    result = result * 10 + i + 1;
-                    break;
-                }
-            }
-        }
-        return Integer.toString(result);
-    }
-}
-
-class Solution2 {
-    public String getPermutation(int n, int k) {
-        int[] fTbl = new int[n + 1];
-        for (int i = 0; i <= n; i++)
-            fTbl[i] = i < 2 ? 1 : fTbl[i - 1] * i;
-
         return dfs(n, k, new boolean[n], fTbl);
     }
 
@@ -39,5 +18,26 @@ class Solution2 {
             }
         }
         return "";
+    }
+}
+
+class Solution2 {
+    public String getPermutation(int n, int k) {
+        int[] fTbl = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+            fTbl[i] = i < 2 ? 1 : fTbl[i - 1] * i;
+
+        boolean[] visited = new boolean[n];
+        int result = 0;
+        for (; n > 0; k = (k - 1) % fTbl[n - 1] + 1, n--) {
+            for (int i = 0, seg = (k - 1) / fTbl[n - 1]; i < visited.length; i++) {
+                if (!visited[i] && --seg < 0) {
+                    visited[i] = true;
+                    result = result * 10 + i + 1;
+                    break;
+                }
+            }
+        }
+        return Integer.toString(result);
     }
 }
