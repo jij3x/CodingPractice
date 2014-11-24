d17af46831c97e2964abcf0096dfa8a53381a9df
jij3x <jijex@yahoo.com>
2014-11-23 20:45:05 -0800

    Update Edit Distance.java

--- LeetCode/Edit Distance/Edit Distance.java
+++ LeetCode/Edit Distance/Edit Distance.java
@@ -24,17 +24,17 @@ public class Solution {
 class Solution2 {
     public int minDistance(String word1, String word2) {
         int[][] memo = new int[word1.length() + 1][word2.length() + 1];
-        for (int i = 0; i <= word1.length(); i++)
-            memo[i][word2.length()] = word1.length() - i;
-        for (int j = 0; j <= word2.length(); j++)
-            memo[word1.length()][j] = word2.length() - j;
+        for (int i = 0, j = word1.length(); i < word1.length(); i++, j--)
+            memo[i][word2.length()] = j;
+        for (int i = 0, j = word2.length(); i < word2.length(); i++, j--)
+            memo[word1.length()][i] = j;
 
         for (int i = word1.length() - 1; i >= 0; i--) {
             for (int j = word2.length() - 1; j >= 0; j--) {
                 if (word1.charAt(i) == word2.charAt(j))
                     memo[i][j] = memo[i + 1][j + 1];
                 else
-                    memo[i][j] = Math.min(memo[i + 1][j], Math.min(memo[i][j + 1], memo[i + 1][j + 1])) + 1;
+                    memo[i][j] = Math.min(Math.min(1 + memo[i + 1][j], 1 + memo[i][j + 1]), 1 + memo[i + 1][j + 1]);
             }
         }
         return memo[0][0];
