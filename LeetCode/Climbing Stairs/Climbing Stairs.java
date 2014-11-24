5f461a27b7dc76f178d9818db73812adcdf6a897
jij3x <jijex@yahoo.com>
2014-11-23 21:03:56 -0800

    Update Climbing Stairs.java

--- LeetCode/Climbing Stairs/Climbing Stairs.java
+++ LeetCode/Climbing Stairs/Climbing Stairs.java
@@ -13,17 +13,15 @@ public class Solution {
 class Solution2 {
     public int climbStairs(int n) {
         int[] memo = new int[n + 1];
-        Arrays.fill(memo, -1);
         return dpClimbStairs(n, memo);
     }
 
     private int dpClimbStairs(int n, int[] memo) {
-        if (n == 0 || n == 1)
+        if (n <= 1)
             return 1;
 
-        if (memo[n] == -1)
+        if (memo[n] == 0)
             memo[n] = dpClimbStairs(n - 1, memo) + dpClimbStairs(n - 2, memo);
-
         return memo[n];
     }
 }
