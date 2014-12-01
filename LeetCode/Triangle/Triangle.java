9479f02095dce7c54cc2ffb30f3ec2385a0e53dd
jij3x <jijex@yahoo.com>
2014-11-30 17:48:13 -0800

    Update Triangle.java

--- LeetCode/Triangle/Triangle.java
+++ LeetCode/Triangle/Triangle.java
@@ -8,3 +8,21 @@ public class Solution {
         return memo[0];
     }
 }
+
+class Solution2 {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        Integer[] curr = new Integer[triangle.size()], prev = new Integer[triangle.size()];
+        prev[0] = triangle.get(0).get(0);
+        for (int i = 1; i < triangle.size(); i++) {
+            curr[0] = prev[0] + triangle.get(i).get(0);
+            curr[i] = prev[i - 1] + triangle.get(i).get(i);
+            for (int j = 1; j < i; j++)
+                curr[j] = Math.min(prev[j - 1], prev[j]) + triangle.get(i).get(j);
+
+            Integer[] temp = curr;
+            curr = prev;
+            prev = temp;
+        }
+        return Collections.min(Arrays.asList(prev));
+    }
+}
