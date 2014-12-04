f1844e86c40b85d25965474ef467db4716b2d9fd
jij3x <jijex@yahoo.com>
2014-12-03 22:14:00 -0800

    Update Best Time to Buy and Sell Stock III.java

--- LeetCode/Best Time to Buy and Sell Stock III/Best Time to Buy and Sell Stock III.java
+++ LeetCode/Best Time to Buy and Sell Stock III/Best Time to Buy and Sell Stock III.java
@@ -3,23 +3,21 @@ public class Solution {
         if (prices.length < 2)
             return 0;
 
-        int[] maxOnLeft = new int[prices.length];
+        int[] leftMax = new int[prices.length];
         for (int i = 1, lowest = prices[0]; i < prices.length; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], prices[i] - lowest);
             lowest = Math.min(lowest, prices[i]);
-            maxOnLeft[i] = Math.max(maxOnLeft[i - 1], prices[i] - lowest);
         }
 
-        int[] maxOnRight = new int[prices.length];
+        int[] rightMax = new int[prices.length];
         for (int i = prices.length - 2, highest = prices[prices.length - 1]; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], highest - prices[i]);
             highest = Math.max(highest, prices[i]);
-            maxOnRight[i] = Math.max(maxOnRight[i + 1], highest - prices[i]);
         }
 
-        int max = 0;
-        for (int i = 1; i < prices.length; i++) {
-            max = Math.max(max, maxOnLeft[i - 1] + maxOnRight[i]);
-        }
-
-        return Math.max(max, Math.max(maxOnLeft[prices.length - 1], maxOnRight[0]));
+        int maxDiff = leftMax[leftMax.length - 1];
+        for (int i = 1; i < prices.length - 2; i++)
+            maxDiff = Math.max(maxDiff, leftMax[i] + rightMax[i + 1]);
+        return maxDiff;
     }
 }
