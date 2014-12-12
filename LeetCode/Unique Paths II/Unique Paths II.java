c5047e76495023920e3018a4e342970b32d9fd92
jij3x <jijex@yahoo.com>
2014-12-11 21:54:41 -0800

    Update Unique Paths II.java

--- LeetCode/Unique Paths II/Unique Paths II.java
+++ LeetCode/Unique Paths II/Unique Paths II.java
@@ -1,12 +1,10 @@
 public class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m = obstacleGrid.length;
-        int n = obstacleGrid[0].length;
-        int[][] memo = new int[m + 1][n + 1];
-        memo[m][n - 1] = 1;
-        for (int y = m - 1; y >= 0; y--) {
-            for (int x = n - 1; x >= 0; x--)
-                memo[y][x] = obstacleGrid[y][x] == 1 ? 0 : memo[y][x + 1] + memo[y + 1][x];
+        int[][] memo = new int[obstacleGrid.length + 1][obstacleGrid[0].length + 1];
+        memo[memo.length - 1][memo[0].length - 2] = 1;
+        for (int y = memo.length - 2; y >= 0; y--) {
+            for (int x = memo[0].length - 2; x >= 0; x--)
+                memo[y][x] = obstacleGrid[y][x] == 1 ? 0 : memo[y + 1][x] + memo[y][x + 1];
         }
         return memo[0][0];
     }
