5868e9f0f4454fbb5880a3c48b777346935a746f
jij3x <jijex@yahoo.com>
2014-12-25 15:12:24 -0800

    Update Search Insert Position.java

--- LeetCode/Search Insert Position/Search Insert Position.java
+++ LeetCode/Search Insert Position/Search Insert Position.java
@@ -3,12 +3,12 @@ public class Solution {
         int start = 0, end = A.length - 1;
         while (start <= end) {
             int mid = (start + end) / 2;
-            if (A[mid] == target)
-                return mid;
+            if (A[mid] > target)
+                end = mid - 1;
             else if (A[mid] < target)
                 start = mid + 1;
             else
-                end = mid - 1;
+                return mid;
         }
         return start;
     }
