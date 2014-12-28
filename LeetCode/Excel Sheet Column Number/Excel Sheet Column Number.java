007c4b3fbacb2235e5c8d8a33e044fc7068c20c0
jij3x <jijex@yahoo.com>
2014-12-28 08:37:08 -0800

    upload

---
+++ LeetCode/Excel Sheet Column Number/Excel Sheet Column Number.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int titleToNumber(String s) {
+        int r = 0;
+        for (int i = 0; i < s.length(); i++)
+            r = r * 26 + (s.charAt(i) - 'A' + 1);
+        return r;
+    }
+}
