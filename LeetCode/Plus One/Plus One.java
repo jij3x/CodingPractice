204dde3e83bee1492464850ee99824557116e034
jij3x <jijex@yahoo.com>
2014-12-28 09:02:18 -0800

    Update Plus One.java

--- LeetCode/Plus One/Plus One.java
+++ LeetCode/Plus One/Plus One.java
@@ -1,20 +1,19 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
-        int[] buf = new int[digits.length];
+        int[] result = new int[digits.length];
         int carry = 1;
-
         for (int i = digits.length - 1; i >= 0; i--) {
-            buf[i] += digits[i] + carry;
-            carry = buf[i] / 10;
-            buf[i] %= 10;
+            result[i] = digits[i] + carry;
+            carry = result[i] / 10;
+            result[i] %= 10;
         }
 
         if (carry == 0)
-            return buf;
-        
-        int[] buf2 = new int[buf.length + 1];
-        buf2[0] = carry;
-        System.arraycopy(buf, 0, buf2, 1, buf.length);
-        return buf2;
+            return result;
+
+        int[] result2 = new int[digits.length + 1];
+        result2[0] = carry;
+        System.arraycopy(result, 0, result2, 1, result.length);
+        return result2;
     }
 }
