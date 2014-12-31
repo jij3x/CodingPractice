b98830235461325732fb6833cd26f4e45867c2d4
jij3x <jijex@yahoo.com>
2014-12-30 22:15:38 -0800

    Update Binary Search Tree Iterator.java

--- LeetCode/Binary Search Tree Iterator/Binary Search Tree Iterator.java
+++ LeetCode/Binary Search Tree Iterator/Binary Search Tree Iterator.java
@@ -47,3 +47,35 @@ public class BSTIterator {
  * BSTIterator i = new BSTIterator(root);
  * while (i.hasNext()) v[f()] = i.next();
  */
+
+public class BSTIterator {
+    private Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<TreeNode>();
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode curr = stack.pop();
+        int result = curr.val;
+
+        if (curr.right != null) {
+            curr = curr.right;
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+        }
+        return result;
+    }
+}
