02f9493a7c8321d296dee0797b033cc2bb68c08c
jij3x <jijex@yahoo.com>
2015-01-02 20:05:55 -0800

    Update Unique Binary Search Trees II.java

--- LeetCode/Unique Binary Search Trees II/Unique Binary Search Trees II.java
+++ LeetCode/Unique Binary Search Trees II/Unique Binary Search Trees II.java
@@ -9,25 +9,23 @@
  */
 public class Solution {
     public List<TreeNode> generateTrees(int n) {
-        return doGenerate(1, n);
+        return dfsGenerateTrees(1, n);
     }
 
-    private List<TreeNode> doGenerate(int start, int end) {
-        List<TreeNode> result = new ArrayList<TreeNode>();
-        if (start > end) {
+    private List<TreeNode> dfsGenerateTrees(int start, int end) {
+        ArrayList<TreeNode> result = new ArrayList<TreeNode>();
+        if (start > end)
             result.add(null);
-            return result;
-        }
 
         for (int i = start; i <= end; i++) {
-            List<TreeNode> leftSet = doGenerate(start, i - 1);
-            List<TreeNode> rightSet = doGenerate(i + 1, end);
+            List<TreeNode> leftTrees = dfsGenerateTrees(start, i - 1);
+            List<TreeNode> rightTrees = dfsGenerateTrees(i + 1, end);
 
-            for (TreeNode leftNode : leftSet) {
-                for (TreeNode rightNode : rightSet) {
+            for (TreeNode leftRoot : leftTrees) {
+                for (TreeNode rightRoot : rightTrees) {
                     TreeNode root = new TreeNode(i);
-                    root.left = leftNode; // Need deep copy?
-                    root.right = rightNode; // Need deep copy?
+                    root.left = leftRoot; // Need deep copy?
+                    root.right = rightRoot; // Need deep copy?
                     result.add(root);
                 }
             }
