6a4ae4c024d2f48ad5f057c96be436e669182959
jij3x <jijex@yahoo.com>
2015-01-04 17:12:47 -0800

    Update Binary Tree Level Order Traversal.java

--- LeetCode/Binary Tree Level Order Traversal/Binary Tree Level Order Traversal.java
+++ LeetCode/Binary Tree Level Order Traversal/Binary Tree Level Order Traversal.java
@@ -76,7 +76,7 @@ class Solution2 {
 class Solution3 {
     public List<List<Integer>> levelOrder(TreeNode root) {
         ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
-        dfs(root, 1, result);
+        dfs(root, 0, result);
         return result;
     }
 
@@ -84,10 +84,10 @@ class Solution3 {
         if (root == null)
             return;
 
-        if (result.size() < level)
+        if (result.size() == level)
             result.add(new ArrayList<Integer>());
 
-        result.get(level - 1).add(root.val);
+        result.get(level).add(root.val);
         dfs(root.left, level + 1, result);
         dfs(root.right, level + 1, result);
     }
