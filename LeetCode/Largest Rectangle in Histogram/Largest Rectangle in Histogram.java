651bee23c8cdd93bdebe53ec02823237ee54b99a
jij3x <jijex@yahoo.com>
2015-01-18 21:30:20 -0800

    Update Largest Rectangle in Histogram.java

--- LeetCode/Largest Rectangle in Histogram/Largest Rectangle in Histogram.java
+++ LeetCode/Largest Rectangle in Histogram/Largest Rectangle in Histogram.java
@@ -1,32 +1,21 @@
 public class Solution {
     public int largestRectangleArea(int[] height) {
-        if (height.length == 0)
-            return 0;
-
-        Stack<Integer> hStack = new Stack<Integer>();
         Stack<Integer> iStack = new Stack<Integer>();
+        Stack<Integer> hStack = new Stack<Integer>();
         int max = 0;
-
-        hStack.push(height[0]);
-        iStack.push(0);
-        for (int i = 1; i < height.length; i++) {
-            if (height[i] < hStack.peek()) {
-                int last = 0;
-                while (!hStack.isEmpty() && hStack.peek() > height[i]) {
-                    last = iStack.pop();
-                    max = Math.max(max, (i - last) * hStack.pop());
-                }
-                hStack.push(height[i]);
-                iStack.push(iStack.isEmpty() ? 0 : last);
-            } else if (height[i] > hStack.peek()) {
-                hStack.push(height[i]);
-                iStack.push(i);
+        for (int i = 0; i < height.length; i++) {
+            int last = i;
+            while (!hStack.isEmpty() && height[i] <= hStack.peek()) {
+                last = iStack.pop();
+                max = Math.max(max, hStack.pop() * (i - last));
             }
+            iStack.push(last);
+            hStack.push(height[i]);
         }
 
-        while (!hStack.isEmpty())
-            max = Math.max(max, hStack.pop() * (height.length - iStack.pop()));
-
+        while (!iStack.isEmpty()) {
+            max = Math.max(max, (height.length - iStack.pop()) * hStack.pop());
+        }
         return max;
     }
 }
