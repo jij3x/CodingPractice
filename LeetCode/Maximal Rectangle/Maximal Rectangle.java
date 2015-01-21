8713381eadd3d2d2346fb59707d595c51908c8cc
jij3x <jijex@yahoo.com>
2015-01-20 23:22:33 -0800

    Update Maximal Rectangle.java

--- LeetCode/Maximal Rectangle/Maximal Rectangle.java
+++ LeetCode/Maximal Rectangle/Maximal Rectangle.java
@@ -8,14 +8,14 @@ public class Solution {
         for (int y = 0; y < m; y++) {
             int[] left = new int[n], right = new int[n];
             for (int x = 0, rx = n - 1; x < n; x++, rx--) {
-                left[x] = matrix[y][x] == '1' ? (x == 0 ? 1 : left[x - 1] + 1) : 0;
-                right[rx] = matrix[y][rx] == '1' ? (rx == n - 1 ? 1 : right[rx + 1] + 1) : 0;
+                left[x] = matrix[y][x] == '0' ? 0 : (x == 0 ? 1 : left[x - 1] + 1);
+                right[rx] = matrix[y][rx] == '0' ? 0 : (rx == n - 1 ? 1 : right[rx + 1] + 1);
             }
 
             for (int x = 0; x < n; x++) {
                 leftMemo[x] = leftMemo[x] == 0 ? left[x] : Math.min(leftMemo[x], left[x]);
                 rightMemo[x] = rightMemo[x] == 0 ? right[x] : Math.min(rightMemo[x], right[x]);
-                heightMemo[x] = matrix[y][x] == '1' ? heightMemo[x] + 1 : 0;
+                heightMemo[x] = matrix[y][x] == '0' ? 0 : heightMemo[x] + 1;
                 max = Math.max(max, heightMemo[x] * (leftMemo[x] + rightMemo[x] - 1));
             }
         }
