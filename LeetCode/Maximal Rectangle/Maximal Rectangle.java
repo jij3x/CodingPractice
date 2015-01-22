0642a360de8d8a0ef2adb1b164497571a8069bb5
jij3x <jijex@yahoo.com>
2015-01-21 22:57:39 -0800

    Update Maximal Rectangle.java

--- LeetCode/Maximal Rectangle/Maximal Rectangle.java
+++ LeetCode/Maximal Rectangle/Maximal Rectangle.java
@@ -6,17 +6,16 @@ public class Solution {
         int m = matrix.length, n = matrix[0].length, max = 0;
         int[] heightMemo = new int[n], leftMemo = new int[n], rightMemo = new int[n];
         for (int y = 0; y < m; y++) {
-            int[] left = new int[n], right = new int[n];
-            for (int x = 0, rx = n - 1; x < n; x++, rx--) {
-                left[x] = matrix[y][x] == '0' ? 0 : (x == 0 ? 1 : left[x - 1] + 1);
-                right[rx] = matrix[y][rx] == '0' ? 0 : (rx == n - 1 ? 1 : right[rx + 1] + 1);
+            for (int x = 0, onesOnLeft = 0; x < n; x++) {
+                heightMemo[x] = matrix[y][x] == '0' ? 0 : heightMemo[x] + 1;
+                leftMemo[x] = heightMemo[x] == 1 ? onesOnLeft : Math.min(leftMemo[x], onesOnLeft);
+                onesOnLeft = heightMemo[x] == 0 ? 0 : onesOnLeft + 1;
             }
 
-            for (int x = 0; x < n; x++) {
-                leftMemo[x] = leftMemo[x] == 0 ? left[x] : Math.min(leftMemo[x], left[x]);
-                rightMemo[x] = rightMemo[x] == 0 ? right[x] : Math.min(rightMemo[x], right[x]);
-                heightMemo[x] = matrix[y][x] == '0' ? 0 : heightMemo[x] + 1;
-                max = Math.max(max, heightMemo[x] * (leftMemo[x] + rightMemo[x] - 1));
+            for (int x = n - 1, onesOnRight = 0; x >= 0; x--) {
+                rightMemo[x] = heightMemo[x] == 1 ? onesOnRight : Math.min(rightMemo[x], onesOnRight);
+                onesOnRight = heightMemo[x] == 0 ? 0 : onesOnRight + 1;
+                max = Math.max(max, heightMemo[x] * (rightMemo[x] + leftMemo[x] + 1));
             }
         }
         return max;
