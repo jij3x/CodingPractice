67875dd0f6ba63b091c29b5ab0908e33f8916e2e
jij3x <jijex@yahoo.com>
2015-01-21 23:35:28 -0800

    Update Reverse Integer.java

--- LeetCode/Reverse Integer/Reverse Integer.java
+++ LeetCode/Reverse Integer/Reverse Integer.java
@@ -1,9 +1,9 @@
 public class Solution {
     public int reverse(int x) {
         int result = 0;
-        while (x != 0) {
-            result = result * 10 + x % 10;
-            x /= 10;
+        for (int r = 0; x != 0; x /= 10, result = r) {
+            if (((r = r * 10 + x % 10) - x % 10) / 10 != result)
+                return 0;
         }
         return result;
     }
