d8ff1dca462297230e1534976770869bf9268062
jij3x <jijex@yahoo.com>
2015-01-24 08:05:16 -0800

    Update Set Matrix Zeroes.java

--- LeetCode/Set Matrix Zeroes/Set Matrix Zeroes.java
+++ LeetCode/Set Matrix Zeroes/Set Matrix Zeroes.java
@@ -1,19 +1,22 @@
 public class Solution {
     public void setZeroes(int[][] matrix) {
         boolean clear1stRow = false, clear1stCol = false;
-        for (int y = 0; y < matrix.length; y++)
+        for (int y = 0; y < matrix.length; y++) {
             if (matrix[y][0] == 0)
                 clear1stCol = true;
-        for (int x = 0; x < matrix[0].length; x++)
+        }
+        for (int x = 0; x < matrix[0].length; x++) {
             if (matrix[0][x] == 0)
                 clear1stRow = true;
+        }
 
         for (int y = 1; y < matrix.length; y++) {
-            for (int x = 1; x < matrix[0].length; x++)
+            for (int x = 1; x < matrix[0].length; x++) {
                 if (matrix[y][x] == 0) {
                     matrix[y][0] = 0;
                     matrix[0][x] = 0;
                 }
+            }
         }
 
         for (int y = 1; y < matrix.length; y++) {
@@ -22,19 +25,20 @@ public class Solution {
                     matrix[y][x] = 0;
             }
         }
-
-        for (int x = 1; x < matrix[0].length; x++)
+        for (int x = 1; x < matrix[0].length; x++) {
             if (matrix[0][x] == 0) {
                 for (int y = 1; y < matrix.length; y++)
                     matrix[y][x] = 0;
             }
+        }
 
-        if (clear1stRow)
+        if (clear1stRow) {
             for (int x = 0; x < matrix[0].length; x++)
                 matrix[0][x] = 0;
-
-        if (clear1stCol)
+        }
+        if (clear1stCol) {
             for (int y = 0; y < matrix.length; y++)
                 matrix[y][0] = 0;
+        }
     }
 }
