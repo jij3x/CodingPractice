f8d7b1585014a0a2f16b6e4c87356c1efec615d1
jij3x <jijex@yahoo.com>
2015-01-24 10:12:30 -0800

    Update Valid Sudoku.java

--- LeetCode/Valid Sudoku/Valid Sudoku.java
+++ LeetCode/Valid Sudoku/Valid Sudoku.java
@@ -1,18 +1,14 @@
 public class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rowMark = new boolean[9][9];
-        boolean[][] colMark = new boolean[9][9];
-        boolean[][] blkMark = new boolean[9][9];
+        boolean[][] rowMemo = new boolean[9][9], columnMemo = new boolean[9][9], blockMemo = new boolean[9][9];
         for (int y = 0; y < 9; y++) {
             for (int x = 0; x < 9; x++) {
-                if (board[y][x] == '.')
-                    continue;
-
-                int num = board[y][x] - '1';
-                if (rowMark[y][num] || colMark[x][num] || blkMark[y / 3 * 3 + x / 3][num])
-                    return false;
-
-                rowMark[y][num] = colMark[x][num] = blkMark[y / 3 * 3 + x / 3][num] = true;
+                if (board[y][x] != '.') {
+                    int i = board[y][x] - '1';
+                    if (rowMemo[y][i] || columnMemo[x][i] || blockMemo[y / 3 * 3 + x / 3][i])
+                        return false;
+                    rowMemo[y][i] = columnMemo[x][i] = blockMemo[y / 3 * 3 + x / 3][i] = true;
+                }
             }
         }
         return true;
