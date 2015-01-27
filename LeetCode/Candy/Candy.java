70b3ae6409aca39304753f81a5828f2502a1b9e7
jij3x <jijex@yahoo.com>
2015-01-26 22:39:57 -0800

    Update Candy.java

--- LeetCode/Candy/Candy.java
+++ LeetCode/Candy/Candy.java
@@ -1,19 +1,17 @@
 public class Solution {
     public int candy(int[] ratings) {
-        int[] count = new int[ratings.length];
-        count[0] = 1;
-
+        int[] memo = new int[ratings.length];
+        memo[0] = 1;
         for (int i = 1; i < ratings.length; i++) {
-            count[i] = ratings[i] > ratings[i - 1] ? count[i - 1] + 1 : 1;
-        }
-        for (int i = ratings.length - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1] && count[i] <= count[i + 1])
-                count[i] = count[i + 1] + 1;
+            memo[i] = ratings[i] > ratings[i - 1] ? memo[i - 1] + 1 : 1;
         }
 
-        int sum = 0;
-        for (int c : count)
-            sum += c;
+        int sum = memo[ratings.length - 1];
+        for (int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && memo[i] <= memo[i + 1])
+                memo[i] = memo[i + 1] + 1;
+            sum += memo[i];
+        }
         return sum;
     }
 }
