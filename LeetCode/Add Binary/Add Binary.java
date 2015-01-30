bb08d9bd0eb01e69f52fe5b50b992f4e375d43d8
jij3x <jijex@yahoo.com>
2015-01-29 22:04:05 -0800

    Update Add Binary.java

--- LeetCode/Add Binary/Add Binary.java
+++ LeetCode/Add Binary/Add Binary.java
@@ -1,9 +1,9 @@
 public class Solution {
     public String addBinary(String a, String b) {
         StringBuilder result = new StringBuilder();
-        for (int i = a.length() - 1, j = b.length() - 1, carry = 0; i >= 0 || j >= 0 || carry > 0; i--, j--) {
+        for (int carry = 0, i = a.length() - 1, j = b.length() - 1; carry > 0 || i >= 0 || j >= 0; i--, j--) {
             carry += (i >= 0 ? a.charAt(i) - '0' : 0) + (j >= 0 ? b.charAt(j) - '0' : 0);
-            result.insert(0, (char) (carry % 2 + '0'));
+            result.insert(0, carry % 2);
             carry /= 2;
         }
         return result.toString();
