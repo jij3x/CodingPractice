641eec52935d39af339082b71eac374bf3a77c34
jij3x <jijex@yahoo.com>
2015-01-31 21:49:47 -0800

    Update Multiply Strings.java

--- LeetCode/Multiply Strings/Multiply Strings.java
+++ LeetCode/Multiply Strings/Multiply Strings.java
@@ -1,19 +1,18 @@
 public class Solution {
     public String multiply(String num1, String num2) {
         int[] buffer = new int[num1.length() + num2.length()];
-        for (int i = num1.length() - 1; i >= 0; i--) {
-            for (int j = num2.length() - 1; j >= 0; j--) {
-                buffer[i + j + 1] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+        for (int i = num2.length() - 1; i >= 0; i--) {
+            for (int j = num1.length() - 1; j >= 0; j--) {
+                buffer[i + j + 1] += (num2.charAt(i) - '0') * (num1.charAt(j) - '0');
                 buffer[i + j] += buffer[i + j + 1] / 10;
                 buffer[i + j + 1] %= 10;
             }
         }
 
         StringBuilder result = new StringBuilder();
-        for (int i = 0, sp = 0; i < buffer.length; i++) {
-            sp += buffer[i] == 0 ? 0 : 1;
-            if (sp != 0)
-                result.append((char) (buffer[i] + '0'));
+        for (int i = 0, noneZeros = 0; i < buffer.length; i++) {
+            noneZeros += buffer[i] == 0 ? 0 : 1;
+            result.append(noneZeros > 0 ? (char) (buffer[i] + '0') : "");
         }
         return result.length() == 0 ? "0" : result.toString();
     }
