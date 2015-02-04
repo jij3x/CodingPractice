540a86e19076843a4bac34763daf9d5d49f56c23
jij3x <jijex@yahoo.com>
2015-02-03 22:32:23 -0800

    Update Valid Palindrome.java

--- LeetCode/Valid Palindrome/Valid Palindrome.java
+++ LeetCode/Valid Palindrome/Valid Palindrome.java
@@ -2,9 +2,9 @@ public class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase();
         for (int start = 0, end = s.length() - 1; start < end;) {
-            if (!Character.isLetter(s.charAt(start)) && !Character.isDigit(s.charAt(start)))
+            if (!Character.isLetterOrDigit(s.charAt(start)))
                 start++;
-            else if (end >= 0 && !Character.isLetter(s.charAt(end)) && !Character.isDigit(s.charAt(end)))
+            else if (!Character.isLetterOrDigit(s.charAt(end)))
                 end--;
             else if (s.charAt(start++) != s.charAt(end--))
                 return false;
