d361f16ea36f1267b5ccbaa6c3f032bc4d2738c8
jij3x <jijex@yahoo.com>
2015-02-07 21:27:16 -0800

    Update Swap Nodes in Pairs.java

--- LeetCode/Swap Nodes in Pairs/Swap Nodes in Pairs.java
+++ LeetCode/Swap Nodes in Pairs/Swap Nodes in Pairs.java
@@ -13,11 +13,11 @@ public class Solution {
     public ListNode swapPairs(ListNode head) {
         ListNode start = new ListNode(0);
         start.next = head;
-        for (ListNode pivot = start; pivot.next != null && pivot.next.next != null; pivot = pivot.next.next) {
-            ListNode node1 = pivot.next, node2 = pivot.next.next;
-            node1.next = node2.next;
-            node2.next = node1;
-            pivot.next = node2;
+        for (ListNode tail = start; tail.next != null && tail.next.next != null; tail = tail.next.next) {
+            ListNode next = tail.next, nextNext = tail.next.next;
+            next.next = nextNext.next;
+            nextNext.next = next;
+            tail.next = nextNext;
         }
         return start.next;
     }
