980e9c1ff0446b8aa6443b96101b24e60b2e6039
jij3x <jijex@yahoo.com>
2015-02-11 21:43:47 -0800

    Update Merge k Sorted Lists.java

--- LeetCode/Merge k Sorted Lists/Merge k Sorted Lists.java
+++ LeetCode/Merge k Sorted Lists/Merge k Sorted Lists.java
@@ -10,26 +10,26 @@
  * }
  */
 public class Solution {
-    public ListNode mergeKLists(ArrayList<ListNode> lists) {
-        for (int cnt = lists.size(); cnt >= 2; cnt = cnt / 2 + cnt % 2) {
-            for (int i = 0; i < cnt / 2; i++) {
-                ListNode start = new ListNode(0);
-                ListNode tail = start, list1 = lists.get(i), list2 = lists.get(cnt - 1 - i);
-                while (list1 != null && list2 != null) {
-                    if (list1.val < list2.val) {
-                        tail.next = list1;
-                        list1 = list1.next;
+    public ListNode mergeKLists(List<ListNode> lists) {
+        for (int end = lists.size() - 1; end > 0; end /= 2) {
+            for (int i = end / 2 + 1, j = 0; i <= end; i++, j++) {
+                ListNode start = new ListNode(0), tail = start;
+                ListNode l1 = lists.get(i), l2 = lists.get(j);
+                while (l1 != null && l2 != null) {
+                    if (l1.val < l2.val) {
+                        tail.next = l1;
+                        l1 = l1.next;
                     } else {
-                        tail.next = list2;
-                        list2 = list2.next;
+                        tail.next = l2;
+                        l2 = l2.next;
                     }
                     tail = tail.next;
                 }
-                tail.next = list1 == null ? list2 : list1;
+                tail.next = l1 == null ? l2 : l1;
 
-                lists.set(i, start.next);
+                lists.set(j, start.next);
             }
         }
-        return lists.size() == 0 ? null : lists.get(0);
+        return lists.isEmpty() ? null : lists.get(0);
     }
 }
