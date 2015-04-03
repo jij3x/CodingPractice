f5c802b790ced87f17d5b8a46edba79119efe612
jij3x <jijex@yahoo.com>
2015-04-02 18:48:10 -0700

    Update gen_java_driver.py

--- mpl/gen_java_driver.py
+++ mpl/gen_java_driver.py
@@ -2,123 +2,94 @@ import sys
 import json
 import re
 
-metadata_fname = sys.argv[1]
-mapper_fname = "Mapper.java"
-drv_tml_fname = "drv_template.java"
-interface_fname = "Solution.java"
+import intfdesc as md
+import typemap as tm
 
-type_map = {
-    ":void": {
-        "javaType": ""
-    },
-    ":int": {
-        "javaType": "int",
-        "serializer": "serializeInt",
-        "seperator": "// |:int"
-    },
-    ":array<:int>": {
-        "javaType": "int[]",
-        "serializer": "serializeIntArray",
-        "seperator": "// |:intArray"
-    },
-    ":list<:int>": {
-        "javaType": "List<Integer>",
-        "serializer": "serializeIntList",
-        "seperator": "// |:intList"
-    }
-}
+tim = tm.type_map
+
+MAPPER_FNM = "Mapper.java"
+DRVTML_FNM = "drv_template.java"
+
+CODE_NAME = "codeName"
+RETURN_CN = "_RETURN_"
+PARAM_CN = "_PARAM_{}_"
+INJECT_POS = "// inject here"
 
 #
 # Read problem description, and compose metadata
 #
-with open(metadata_fname) as metadata_file:
+with open(sys.argv[1]) as metadata_file:
     metadata = json.load(metadata_file)
-_function = metadata["interface"]["functionName"]
-_params = metadata["interface"]["paramList"]
-_output = metadata["output"]
-for index, param in enumerate(_params):
-    _params[index]["codeName"] = "_PARAM_{}_".format(str(index))
-if "return" in metadata["interface"]:
-    metadata["interface"]["return"]["codeName"] = "_RETURN_"
+_function = metadata[md.itf][md.fnn]
+_params = metadata[md.itf][md.pl]
+_return = metadata[md.itf][md.rt]
+_output = metadata[md.out]
+for i, pm in enumerate(_params):
+    _params[i][CODE_NAME] = PARAM_CN.format(str(i))
+if md.rt in metadata[md.itf]:
+    metadata[md.itf][md.rt][CODE_NAME] = RETURN_CN
 
 #
 # Go to inject position in Driver template
 #
-driver_src = ""
+driver_code = ""
 driver_tail = ""
-with open(drv_tml_fname) as driver_template:
+with open(DRVTML_FNM) as driver_template:
     is_tail = False
     for line in driver_template:
-        if line.strip() == "// inject here":
+        if line.strip() == INJECT_POS:
             is_tail = True
         elif not is_tail:
-            driver_src += line
+            driver_code += line
         else:
             driver_tail += line
 
 
-def fetch_param_code(template_fname, param):
+def fetch_param_deser_code(template_fname, param):
     param_code = ""
 
     with open(template_fname) as mapper_file:
         regex = re.compile(r"_PARAM_")
         saw_seperator = False
         for line in mapper_file:
-            if line.strip() == type_map[param["type"]]["seperator"]:
+            if line.strip() == tim[param[md.tp]][tm.k_sep]:
                 if saw_seperator:
                     break
                 saw_seperator = True
             elif saw_seperator:
-                line = regex.sub(param["codeName"], line)
+                line = regex.sub(param[CODE_NAME], line)
                 param_code += line
 
     return param_code
 
 #
-# Inject parameters deserialization
+# Inject parameters deserialization code
 #
-for index, param in enumerate(_params):
-    driver_src += fetch_param_code(mapper_fname, param) + "\n"
+for i, pm in enumerate(_params):
+    driver_code += fetch_param_deser_code(MAPPER_FNM, pm) + "\n"
 
 #
 # Compose the code to call Solution
 #
 inputs = ""
-for index, param in enumerate(_params):
-    inputs += (", " if index > 0 else "") + _params[index]["codeName"]
+for i, pm in enumerate(_params):
+    inputs += (", " if i > 0 else "") + _params[i][CODE_NAME]
 return_param = ""
-if "return" in metadata["interface"]:
-    _return = metadata["interface"]["return"]
-    return_param = "{} {} = ".format(type_map[_return["type"]]["javaType"], _return["codeName"])
-driver_src += " " * 12 + "{}(new Solution()).{}({});\n".format(return_param, _function, inputs)
+if md.rt in metadata[md.itf]:
+    _return = metadata[md.itf][md.rt]
+    return_param = "{} {} = ".format(tim[_return[md.tp]][tm.k_java_t], _return[CODE_NAME])
+driver_code += " " * 12 + "{}(new Solution()).{}({});\n".format(return_param, _function, inputs)
 
 #
-# Serialize the Solution output
+# Serialize the output from solution
 #
-inputs = eval("metadata['interface']" + _output["from"] + "['codeName']")
-inputs += ', "{}"'.format(_output["overrideSize"] if "overrideSize" in _output else "none")
-serializer = type_map[eval("metadata['interface']" + _output["from"] + "['type']")]["serializer"]
-driver_src += " " * 12 + "printWriter.println(Serializer.{}({}));\n".format(serializer, inputs)
+inputs = eval("{}['{}']{}['{}']".format("metadata", md.itf, _output[md.sc], CODE_NAME))
+inputs += ', "{}"'.format(_output[md.osz] if md.osz in _output else md.non)
+serializer = tim[eval("{}['{}']{}['{}']".format("metadata", md.itf, _output[md.sc], md.tp))][tm.k_ser]
+driver_code += " " * 12 + "printWriter.println(Serializer.{}({}));\n".format(serializer, inputs)
 
 #
-# Driver.java is for returning
+# Driver.java is ready for returning
 #
-driver_src += driver_tail
-print(driver_src)
-
-#
-# Create Solution stub, and write to Solution.java
-#
-with open(interface_fname, "w") as interface_file:
-    _interface = metadata["interface"]
-    return_type = type_map[_interface["return"]["type"]]["javaType"] if "return" in _interface else "void"
-    inputs = ""
-    for index, param in enumerate(_params):
-        inputs += (", " if index > 0 else "") + type_map[param["type"]]["javaType"] + " " + param["name"]
-    interface_file.write('''
-public class Solution {{
-    public {} {}({}) {{
-
-    }}
-}}
-'''.format(return_type, _function, inputs))
+driver_code += driver_tail
+print(driver_code)
