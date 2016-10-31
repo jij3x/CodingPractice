9e2c8e8f446af4a123cf0015b05a9ddd76aa5e0e
jij3x <jijex@yahoo.com>
2016-10-30 21:27:28 -0700

    update

--- LeetCode/_NewStart/375 Guess Number Higher or Lower II/GuessNumberHigherOrLowerII.java
+++ LeetCode/_NewStart/375 Guess Number Higher or Lower II/GuessNumberHigherOrLowerII.java
@@ -20,3 +20,22 @@ public class Solution {
   }
 }
 
+public class Solution2 {
+  public int getMoneyAmount(int n) {
+    if (n == 1) {
+      return 0;
+    }
+
+    int[][] dp = new int[n + 1][n + 1];
+    for (int right = 1; right <= n; right++) {
+      for (int left = right; left >= 1; left--) {
+        dp[left][right] = Integer.MAX_VALUE;
+        for (int i = right; i >= left; i--) {
+          dp[left][right] = Math.min(dp[left][right],
+              i + Math.max(left >= i - 1 ? 0 : dp[left][i - 1], i + 1 >= right ? 0 : dp[i + 1][right]));
+        }
+      }
+    }
+    return dp[1][n];
+  }
+}
